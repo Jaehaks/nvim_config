cdca791ee9cd663c8a21f0558e15b68cf673576b
Jaehaks <wogkr3654@naver.com>
2024-02-26 23:09:35 +0900

    new : highlight word on the cursor

---
+++ vim-illuminate.lua
@@ -0,0 +1,44 @@
+return {
+	'RRethy/vim-illuminate',
+	event = 'VeryLazy',
+	config = function()
+
+--		-- highlight color configuration
+--		local bgcolor = '#808080'
+--		local highlights = {
+--			IlluminatewdWord = {bg = bgcolor},
+--			IlluminatewdCurWord = {bg = bgcolor},
+--			IlluminatewdWordText = {bg = bgcolor},
+--			IlluminatewdWordRead = {bg = bgcolor},
+--			IlluminatewdWrite = {bg = bgcolor},
+--		}
+--		for group,value in pairs(highlights) do 
+--			vim.api.nvim_set_hl(0, group, value)
+--		end
+
+
+		-- plugin configuration
+		local illuminate = require('illuminate')
+		illuminate.configure({
+			--providers = {'lsp', 'regex'},
+			providers = {'regex'},	-- if using lsp, entire text in '' is highlighted 
+			delay = 300,
+			lcarge_file_curoff = 8000,
+			filetypes_denylist = {
+				'help',
+				'lir',
+				'TelescopePrompt',
+			},
+			min_count_to_highlight = 2,
+
+		})
+		-- IlluminatedWordText : no kind information word,   like text  (using 'regex' provider)
+		-- IlluminatedWordRead : kind information word,   like variables (using 'lsp' provider)
+		-- for gui,  view :h attr-list
+		-- gui=NONE : delete underline in highlight
+		vim.cmd([[hi IlluminatedWordText guibg=#45535D gui=NONE]])
+		vim.cmd([[hi IlluminatedWordRead guibg=#45535D gui=NONE]])
+		vim.cmd([[hi IlluminatedWrite 	 guibg=#45535D gui=NONE]])
+
+	end
+}
