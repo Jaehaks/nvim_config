440cb10becd87b9a92dc885b70ce8a1a2f9b62c9
Jaehaks <wogkr3654@naver.com>
2024-02-27 22:34:22 +0900

    add : change IncSearch color

--- lua/jaehak/plugins/colorscheme.lua
+++ lua/jaehak/plugins/colorscheme.lua
@@ -48,6 +48,7 @@ return{{
 				vim.api.nvim_set_hl(0, "Function", { fg = "#C587FF"})
 				vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#D6DEEB"})	-- white_blue
 				vim.api.nvim_set_hl(0, "Search", { fg = "#000000", bg = '#FFFF00'})	-- white_blue
+				vim.api.nvim_set_hl(0, "IncSearch", { fg = "#000000", bg = '#F78C6C'})	-- orange
 			end,
 			group = custom_highlight,
 		})
