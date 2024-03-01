cd46c6c97aa0f916f61d9d42cb10dd4bba0fa46e
Jaehaks <Jaehaks@github.com>
2024-03-01 17:55:20 +0900

    new : comment plugin

---
+++ lua/jaehak/plugins/lsp/mini-comment.lua
@@ -0,0 +1,21 @@
+return {
+	'echasnovski/mini.comment',
+	version = '*',
+	event = 'VeryLazy',
+	config = function()
+	    require('mini.comment').setup({
+			options = {
+				ignore_blank_line = false,
+				start_of_line = false, 			-- if true, comment located at SOL
+												-- but when comment out by toggle, the line is indented by 1 char
+			},
+			mappings = {
+				comment = 'gc',			-- for following command, like "gcip"
+				comment_line = 'gl',
+				comment_visual = 'gl',
+				textobject = 'gc', 		-- for preceding command, like "dgc", delete comment region on cursor
+			}
+
+		})
+	end
+}
