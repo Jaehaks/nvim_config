90fb58e639a6ee0aa9ec469fafe3bbcb55be5941
Jaehaks <Jaehaks@github.com>
2024-03-03 23:32:15 +0900

    add : '--' and '%' for other language comment sign

--- lua/jaehak/plugins/lsp/vim-easy-align.lua
+++ lua/jaehak/plugins/lsp/vim-easy-align.lua
@@ -8,6 +8,18 @@ return {
 				left_margin = 1,
 				right_margin = 1,
 				stick_to_left = 0,
+			},
+			['--'] = {				-- add delimiters '--', lua comment
+				pattern = '--',
+				left_margin = 1,
+				right_margin = 1,
+				stick_to_left = 0,
+			},
+			['%'] = {				-- add delimiters '%', matlab comment
+				pattern = '%',
+				left_margin = 1,
+				right_margin = 1,
+				stick_to_left = 0,
 			}
 		}
 		vim.keymap.set({'n', 'x'}, '<C-=>', ':EasyAlign ')
