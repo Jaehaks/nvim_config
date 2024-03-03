f972e6ceb6d41c586b27a98b4f4703352179c016
Jaehaks <Jaehaks@github.com>
2024-03-03 23:31:24 +0900

    add : rainbow-delimiters to colorful brackets

--- lua/jaehak/plugins/lsp/ultimate-autopair.lua
+++ lua/jaehak/plugins/lsp/ultimate-autopair.lua
@@ -6,9 +6,37 @@ return {
 	opts = {},
 },
 {
-	'luochen1990/rainbow',
-	config = function()
-		vim.g.rainbow_active = 1
+	-- auto highlight to brackets
+	'HiPhish/rainbow-delimiters.nvim',
+	event = 'VeryLazy',
+	config = function ()
+		-- This module contains a number of default definitions
+		local rainbow_delimiters = require('rainbow-delimiters')
+
+		-- rainbow_delimiters.config
+		vim.g.rainbow_delimiters = {
+			strategy = {
+				[''] = rainbow_delimiters.strategy['global'],
+				vim = rainbow_delimiters.strategy['local'],
+			},
+			query = {
+				[''] = 'rainbow-delimiters',
+				lua = 'rainbow-blocks',
+			},
+			priority = {
+				[''] = 110,
+				lua = 210,
+			},
+			highlight = {
+				'RainbowDelimiterRed',
+				'RainbowDelimiterYellow',
+				'RainbowDelimiterBlue',
+				'RainbowDelimiterOrange',
+				'RainbowDelimiterGreen',
+				'RainbowDelimiterViolet',
+				'RainbowDelimiterCyan',
+			},
+		}
 	end
 }
 }
