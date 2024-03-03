cc5bacb8dc9adcceed67a92556317b162e389279
Jaehaks <Jaehaks@github.com>
2024-03-03 23:21:49 +0900

    new : improved lsp behavior

---
+++ lua/jaehak/plugins/lsp/lspsaga.lua
@@ -0,0 +1,42 @@
+return {
+	'nvimdev/lspsaga.nvim',		-- improved lsp actions
+	enabled = true,
+	event = 'LspAttach',
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter',
+		'nvim-tree/nvim-web-devicons'
+	},
+	config = function ()
+		require('lspsaga').setup({
+			-- ////// hover : library definition //////////
+			hover = {					-- "KK" makes cursor move into hover window
+				open_link = '<C-]>',   -- it can use website link also.
+				open_cmd = '!chrome'
+			},
+			-- ////// outline : code tree //////////	
+			outline = {
+				layout = 'float',
+				detail = true,
+			},
+			-- ////// lightbulb : code action notify //////// 
+			lightbulb = {
+				enable = false,		-- lightbulb makes screen shake, I don't know why
+			},
+			definition = {
+				keys = {
+					edit = 'o'
+				}
+			},
+			-- ///// floatterm : floating terminal module ////////
+			floaterm = {
+				height = 0.3,
+			}
+		})
+
+		vim.keymap.set('n', '<leader>lo', '<Cmd>Lspsaga outline<CR>', {desc = 'outline', silent = true, noremap = true})
+		vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', {desc = 'hover_doc', silent = true, noremap = true})
+		vim.keymap.set('n', '<C-\\>', '<Cmd>Lspsaga term_toggle<CR>', {desc = 'toggle term', silent = true, noremap = true})
+		-- toggle term must close using :q! not :q
+		-- lspsaga toggle term can use after lsp attatch
+	end
+}
