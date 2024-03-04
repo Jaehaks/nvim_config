c89c0d55b585f54d8487949be164411bf3095f71
Jaehaks <Jaehaks@github.com>
2024-03-04 22:47:00 +0900

    modify : set keymaps to comment

--- lua/jaehak/plugins/lsp/comment.lua
+++ lua/jaehak/plugins/lsp/comment.lua
@@ -2,8 +2,24 @@ return {
 	'numToStr/Comment.nvim',
 	config = function()
 		local ft = require('Comment.ft')
-		ft.set('matlab', {'%\t\t%s', '%{ %s %}'})
-		require('Comment').setup()
+--		ft.set('matlab', {'%%s', '%{%s%}'})
+
+		require('Comment').setup({
+			toggler = {
+				line  = 'gl',
+				block = 'gb',
+			},
+			opleader = {
+				line  = 'gl',
+				block = 'gb',
+			},
+			extra = {
+				above = nil,
+				below = nil,
+				eol   = 'gL'
+			}
+
+		})
 
 	end
 }
