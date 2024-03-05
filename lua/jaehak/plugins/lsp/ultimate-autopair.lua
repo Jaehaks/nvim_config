681fcb582f4f46c70ab6dc7d8de6651287fcbadd
Jaehaks <wogkr3654@naver.com>
2024-03-06 00:24:01 +0900

    modify : for highlight brackets, but rainbow-delimiters works only lua, not matlab

--- lua/jaehak/plugins/lsp/ultimate-autopair.lua
+++ lua/jaehak/plugins/lsp/ultimate-autopair.lua
@@ -3,29 +3,43 @@ return {
 	'altermo/ultimate-autopair.nvim',
 	event = {'InsertEnter', 'CmdlineEnter'},
 	branch = 'v0.6',
-	opts = {},
+	opts = {
+		profile = 'default',
+		multiline = false,
+		bs = {
+			enable = true,
+			overjumps = false,	-- disable (*foo) -> *foo
+			space = false,		-- disable ( *foo ) -> (*foo)
+		},
+		fastwarp = {
+			enable = false,
+		}
+	},
 },
 {
 	-- auto highlight to brackets
 	'HiPhish/rainbow-delimiters.nvim',
+	enabled = false,
 	event = 'VeryLazy',
 	config = function ()
 		-- This module contains a number of default definitions
 		local rainbow_delimiters = require('rainbow-delimiters')
+		local rainbow_delimiters_setup = require('rainbow-delimiters.setup')
 
 		-- rainbow_delimiters.config
-		vim.g.rainbow_delimiters = {
+		rainbow_delimiters_setup.setup({
 			strategy = {
 				[''] = rainbow_delimiters.strategy['global'],
-				vim = rainbow_delimiters.strategy['local'],
-			},
-			query = {
-				[''] = 'rainbow-delimiters',
-				lua = 'rainbow-blocks',
+				matlab = rainbow_delimiters.strategy['global'],
 			},
 			priority = {
 				[''] = 110,
 				lua = 210,
+				matlab = 310,
+			},
+			query = {
+				[''] = 'rainbow-delimiters',
+				matlab = 'rainbow-delimiters',
 			},
 			highlight = {
 				'RainbowDelimiterRed',
@@ -36,7 +50,18 @@ return {
 				'RainbowDelimiterViolet',
 				'RainbowDelimiterCyan',
 			},
-		}
+		})
+	end
+},
+{
+	'andymass/vim-matchup',
+	enabled = true,
+	event = 'VeryLazy',
+	config = function ()
+		require('match-up').setup({})
+		vim.g.loaded_matchit = 1
+		vim.matchup_matchparen_enabled = 1
+		vim.g.matchup_matchparen_offscreen = {method = 'popup'}
 	end
 }
 }
