5f967e98b57b23252d85dac19e3da2aa6be60cf6
Jaehaks <Jaehaks@github.com>
2024-03-08 22:48:01 +0900

    modify : change keymaps

--- lua/jaehak/plugins/lsp/nvim-surround.lua
+++ lua/jaehak/plugins/lsp/nvim-surround.lua
@@ -1,4 +1,5 @@
 return {
+	-- mini-surround.nvim : only linewise, it has a bug that when a indent is ahead of word, surround inclueds the indent
 	-- quick add/delete/change surrounding pairs
 	'kylechui/nvim-surround',
 	version = '*',
@@ -8,15 +9,15 @@ return {
 			keymaps = {
 				insert          = "<C-g>s",
 				insert_line     = "<C-g>S",
-				normal          = "ys",			-- [linewise] ysiw"(word), ysl', yst;},(from current to jumpted location)
-				normal_cur      = "yss",		-- [linewise] yss"  (current whole line)
-				normal_line     = "yS",			-- [blockwise] ySiw"(word),  
-				normal_cur_line = "ySS",		-- [blockwise] ySS" (current whole line), enter " above and below of cur line 
-				visual          = "S",			-- [linewise] (v)S" left and right pairs of visual block
-				visual_line     = "gS",			-- [blockwise] (v)gS" above and below pairs of visual block
-				delete          = "ds",			-- delete 
-				change          = "cs",			-- change pairs and remain the linewise
-				change_line     = "cS",			-- change pairs and move to blockwise
+				normal          = "sa",		-- [linewise] ysiw"(word), ysl', yst;},(from current to jumpted location)
+				normal_cur      = "sA",		-- [linewise] yss"  (current whole line)
+				normal_line     = "sb",		-- [blockwise] ySiw"(word),
+				normal_cur_line = "sB",		-- [blockwise] ySS" (current whole line), enter " above and below of cur line
+				visual          = "sa",		-- [linewise] (v)S" left and right pairs of visual block
+				visual_line     = "sb",		-- [blockwise] (v)gS" above and below pairs of visual block
+				delete          = "sd",		-- delete
+				change          = "sr",		-- change pairs and remain the linewise
+				change_line     = "sR",		-- change pairs and move to blockwise
 
 				-- all left ([<{  ==> add white space 
 				-- all right )}>] ==> not add white space 
