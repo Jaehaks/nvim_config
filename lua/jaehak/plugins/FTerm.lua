d6155843070a0617f3801ae55b10e74e6d9ea1a8
Jaehaks <wogkr3654@naver.com>
2024-03-14 00:04:49 +0900

    add : Fterm for floating terminal
    add disable_ft to use 'q' instead of ':q!' to quit

---
+++ lua/jaehak/plugins/FTerm.lua
@@ -0,0 +1,23 @@
+return {
+{
+	"numToStr/FTerm.nvim",
+	enabled = true,
+	config = function ()
+		local fterm = require('FTerm')
+		fterm.setup({
+			ft = 'FTerm',
+			border = 'rounded',
+			cmd = 'cmd',
+			dimensions = {
+				height = 0.4,
+				width = 0.9,
+				x = 0.5,
+				y = 0.9,
+			}
+		})
+
+		vim.keymap.set('n', '<leader>t',fterm.toggle, {desc = 'open FTerm'} )
+	end
+},
+-- vim-floaterm : cannot select position in detail, but it seems faster than FTerm
+}
