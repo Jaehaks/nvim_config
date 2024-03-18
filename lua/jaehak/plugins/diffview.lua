3ff9548e522426b5bb1bb3f78430b3fa6adf62df
Jaehaks <wogkr3654@naver.com>
2024-03-18 22:38:27 +0900

    New : Add DiffView.nvim
    with neogit, Diffview will be used
    But it cannot be used to compare other two files

---
+++ lua/jaehak/plugins/diffview.lua
@@ -0,0 +1,27 @@
+return {
+	'sindrets/diffview.nvim',
+	dependencies = {
+		'nvim-tree/nvim-web-devicons'
+	},
+	config = function ()
+		local actions = require('diffview.actions')
+		require('diffview').setup({
+			keymaps = {
+				view = {
+					{'n', 'q', actions.close}
+				},
+				file_panel = {
+					{'n', 'j', actions.prev_entry , {silent = true}},
+					{'n', 'k', actions.next_entry , {silent = true}},
+					{'n', 'q', function()
+						actions.close()		-- close panel 
+						actions.close()		-- close view
+					end, {silent = true}},
+				}
+			}
+		})
+
+		vim.keymap.set('n', '<leader>gd', '<Cmd>DiffviewOpen<CR>', {desc = 'open Diffview of .git with HEAD~1'})
+
+	end
+}
