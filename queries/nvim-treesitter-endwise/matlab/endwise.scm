2684061b57ba6fa7df2599e33468b5ec0f12f30b
Jaehaks <wogkr3654@naver.com>
2024-03-18 22:40:22 +0900

    New : editing matlab query
    to apply nvim-treesittter-endwise

---
+++ queries/nvim-treesitter-endwise/matlab/endwise.scm
@@ -0,0 +1,4 @@
+((function_definition arguments: (_) @cursor) @endable @indent (#endwise! "end"))
+((while_statement) @cursor @endable @indent (#endwise! "end"))
+((for_statement ) @cursor) @endable @indent (#endwise! "end"))
+((if_statement ) @cursor @endable @indent (#endwise! "end"))
