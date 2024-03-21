ed03afd5ce364ea4c3e050b2bc95142a82ec44b9
Jaehaks <wogkr3654@naver.com>
2024-03-21 21:52:44 +0900

    New : add markdown-preview.nvim instead of peek.nvim
    - peek.nvim has some drawbacks
        it can use webview instead of browser. it is very good merits to me
        but it does not work when I open a file from folder directly,
        I don't know what is the reason
        it works only when i open a file in nvim.
    
        So I changed the config to use browser. and it works the first few
        times, But it dose not work also now.
    
        Then I move to markdown-preview. and it works very well and fast

--- lua/jaehak/plugins/lsp/markdown.lua
+++ lua/jaehak/plugins/lsp/markdown.lua
@@ -3,30 +3,42 @@ return {
 	-- markdown preview plugin
 	-- BUG: (app = 'webview' only) if file open directly with double click, peekopen does not work 
 	-- BUG: To execute peekopen function, you have to execute nvim-qt first and open the file lately
-	'toppair/peek.nvim',
-	-- event = {'VeryLazy'},
+	-- 
+	-- BUG: 240319 : open does not work in browser also 
+	-- 'toppair/peek.nvim',
+	-- enabled = false,
+	-- -- event = {'VeryLazy'},
+	-- ft = {'markdown'},
+	-- -- need "deno" => choco install deno 
+	-- -- build = 'deno task --quiet build:fast',
+	-- config = function ()
+	-- 	local peek = require('peek')
+	-- 	peek.setup({
+	-- 		auto_load = true, 	-- load preview when entering markdown buffer
+	-- 		close_on_bdelete = true,  -- close preview on buffer delete
+	-- 		app = 'browser',
+	-- 	})
+	--
+	-- 	-- make keymap
+	-- 	vim.keymap.set('n', '<leader>mp', function ()
+	-- 		if peek.is_open() then
+	-- 			return peek.close()
+	-- 		else
+	-- 			return peek.open()
+	-- 		end
+	-- 	end,
+	-- 	{desc = 'toggle markdown preview'})
+	-- end
+},
+{
+	-- more works better
+	'iamcco/markdown-preview.nvim',
 	ft = {'markdown'},
-	-- need "deno" => choco install deno 
-	-- build = 'deno task --quiet build:fast',
+	build = function() vim.fn["mkdp#util#install"]() end,
 	config = function ()
-		local peek = require('peek')
-		peek.setup({
-			auto_load = true, 	-- load preview when entering markdown buffer
-			close_on_bdelete = true,  -- close preview on buffer delete
-			app = 'browser',
-		})
-
-		-- make keymap
-		vim.keymap.set('n', '<leader>mp', function ()
-			if peek.is_open() then
-				return peek.close()
-			else
-				return peek.open()
-			end
-		end,
-		{desc = 'toggle markdown preview'})
-
-
+		vim.g.mkdp_auto_start = 0 -- open preview automatically
+		vim.g.mkdp_theme = 'dark'
+		vim.keymap.set('n', '<leader>mp', '<Cmd>:MarkdownPreviewToggle<CR>', {desc = 'toggle markdown preview'})
 	end
 },
 {
@@ -57,9 +69,10 @@ return {
 	dependencies = 'nvim-treesitter/nvim-treesitter',
 	config = function ()
 
-		vim.api.nvim_set_hl(0, "Headline1", { bg = "#6F0957", bold = true})                  -- for normal cursor
-		vim.api.nvim_set_hl(0, "Headline2", { bg = "#4C083C", bold = true})                  -- for normal cursor
-		vim.api.nvim_set_hl(0, "Headline3", { bg = "#310627", bold = true})                  -- for normal cursor
+		vim.api.nvim_set_hl(0, "Headline1", { bg = "#195E52", bold = true, fg = '#FFFFFF'})                  -- for normal cursor
+		vim.api.nvim_set_hl(0, "Headline2", { bg = "#3F107B", bold = true, fg = '#FFFFFF'})                  -- for normal cursor
+		vim.api.nvim_set_hl(0, "Headline3", { bg = "#6F0957", bold = true, fg = '#FFFFFF'})                  -- for normal cursor
+		vim.api.nvim_set_hl(0, "Headline4", { bg = "#4D3505", bold = true, fg = '#FFFFFF'})                  -- for normal cursor
 		vim.api.nvim_set_hl(0, "CodeBlock", { bg = "#202020"})                  -- for normal cursor
 		vim.api.nvim_set_hl(0, "Dash", { fg = "#D19A66"})                  -- for normal cursor
 
@@ -69,6 +82,7 @@ return {
 					'Headline1',
 					'Headline2',
 					'Headline3',
+					'Headline4',
 				},
 				bullets = false,		-- don't use marker
 				dash_string = false,
