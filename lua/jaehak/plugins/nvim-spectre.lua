ee9ae3662e716fa6acae97cf7b3b102a60798039
Jaehaks <wogkr3654@naver.com>
2024-04-02 23:21:56 +0900

    feat : add nvim-spectre
    tool for search and replace with window

---
+++ lua/jaehak/plugins/nvim-spectre.lua
@@ -0,0 +1,26 @@
+return {
+	'nvim-pack/nvim-spectre',
+	dependencies = {
+		'nvim-lua/plenary.nvim'
+	},
+	config = function ()
+		require('spectre').setup({
+			mapping = {
+				['run_current_replace'] = {
+					map = "y",
+					cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
+					desc = "replace current line"
+				},
+				['run_replace_all'] = {
+					map = "<C-y>",
+					cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+					desc = "replace all"
+				},
+			}
+
+		})
+
+		-- only current file and word under cursor
+		vim.keymap.set('n', '<leader>sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', { desc = "Search on current file" })
+	end
+}
