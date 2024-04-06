151f02d6af3c7ea9171a823f50e5a89706053af5
Jaehaks <wogkr3654@naver.com>
2024-04-07 00:36:16 +0900

    add : change keymap to move
    Since yankbank is updated, it can support keymap changing.
    1) moving key changed
    2) jump method is changed

--- lua/jaehak/plugins/yankbank.lua
+++ lua/jaehak/plugins/yankbank.lua
@@ -1,13 +1,23 @@
 return {
- -- save yank history
+	-- save yank history
 	'ptdewey/yankbank-nvim',
 	config = function ()
+
 		local yankbank = require('yankbank')
+
 		yankbank.setup({
-			max_entries = 10, -- max history
+			max_entries = 9, -- max history
+			sep = "          ",
+			keymaps = {
+				navigation_next = 'k',
+				navigation_prev = 'j',
+			},
+			num_behavior = 'jump' -- go to the number of list when i enter only number
 		})
+
 		vim.keymap.set("n", "<leader>y", ":YankBank<CR>", { noremap = true })
 	end
+
 }
 
 
