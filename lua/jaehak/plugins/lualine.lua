1b17e1b16310b36c02642478b136c893502a28e4
Jaehaks <wogkr3654@naver.com>
2024-04-07 00:40:51 +0900

    add : add features(branch, search) in statusline

--- lua/jaehak/plugins/lualine.lua
+++ lua/jaehak/plugins/lualine.lua
@@ -1,17 +1,22 @@
 return {
+{
 	'nvim-lualine/lualine.nvim',
+	enabled = true,
 	dependencies = {
 		'nvim-tree/nvim-web-devicons'
 	},
 	config = function()
 		require('lualine').setup({
 			options = {
-				disabled_filetypes = {'NvimTree', 'Trouble', 'alpha', 'noice'},
+				disabled_filetypes = {'oil', 'Trouble', 'alpha'},
 			},
 			sections = {
-				lualine_b = {'filesize'}
+				lualine_b = {'filesize'},
+				lualine_c = {'branch', 'filename', 'diagnostics'},
+				lualine_y = {'searchcount', 'selectioncount', 'progress'}
 			}
 		})
 	end
-
+},
 }
+-- heirline.nvim : it can be more powerful, but lualine has more simple way to configure the same configuration.  
