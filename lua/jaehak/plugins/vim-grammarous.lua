b187e12c8927e7859f32e1a40fd4f946d0bb6e11
Jaehaks <wogkr3654@naver.com>
2024-04-08 00:05:47 +0900

    del : not used

--- lua/jaehak/plugins/vim-grammarous.lua
+++ lua/jaehak/plugins/vim-grammarous.lua
@@ -1,24 +1,35 @@
 return {
+{
 	'rhysd/vim-grammarous',
 	enabled = false,
+	dependencies = {
+		'Shougo/vimproc.vim',
+		'Shougo/unite.vim',
+		'kana/vim-operator-user'
+	},
 	-- vim plugin cannot use lazy load
 	config = function()
-		vim.cmd([[
-			let g:grammarous#default_comments_only_filetypes = {
-				\ '*' : 1,
-				\ 'help' : 0,
-				\ 'markdown' : 0,
-				\ 'text' : 0, 
-				\ }
-			let g:grammarous#disabled_rules = {
-				\ '*' : ['WHITESPACE_RULE', 'EN_QUOTES'],
-				\ 'help' : ['WHITESPACE_RULE', 'EN_QUOTES', 'SENTENCE_WHITESPACE', 'UPPERCASE_SENTENCE_START'],
-				\ }
-			let g:grammarous#languagetool_cmd = 'languagetool'
-		]])
-		-- 3) use "languagetool" from external
-		-- 	  this plugin install languagetool using curl but i installed manually (scoop install languagetool-java)
-
-		vim.keymap.set({'n', 'v'}, '<leader>tg', '<Cmd>GrammarousCheck<CR>', {desc = 'Check grammar'})
+		vim.g['grammarous#default_comments_only_filetypes'] = {
+			['*'] = 1,
+			['help'] = 0,
+		}
+		vim.g['grammarous#languagetool_cmd'] = 'java -jar C:\\Users\\USER\\Documents\\My_installer_file\\LanguageTool-5.9\\languagetool-commandline.jar'
+		vim.keymap.set({'n', 'v'}, '<leader>tg', '<Cmd>GrammarousCheck --lang=en-US <CR>', {desc = 'Check grammar'})
+	end
+},
+{
+	'dpelle/vim-LanguageTool',
+	enabled = false,
+	config = function ()
+		vim.g.languagetool_jar = 'C:\\Users\\USER\\Documents\\My_installer_file\\LanguageTool-5.9\\languagetool-commandline.jar'
 	end
 }
+}
+-- 'dpelle/vim-LanguageTool' : it show grammar error list in temporal buffer,
+-- 							   it works with under LanguageTool-5.9 because of --api option does not exist in latest version
+-- 							   languageTool's grammar is not exact what i want..
+-- 							   I have to modify the grammar error manually
+--
+-- 							   i doesn't change any something.
+-- 'vim-grammarous' : it use languagetool also, Accuracy is too low 
+-- 'grammarly lsp' : documents are shared with them
