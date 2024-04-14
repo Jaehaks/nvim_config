dd8ed144d2f31abcd19aa7f722e75710f8fde09d
Jaehaks <wogkr3654@naver.com>
2024-04-14 15:29:37 +0900

    feat : test files for latex

--- test/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main void (){
-	int a = 1;
-	float 1 = 2 
-}
--- test/test.lua
+++
@@ -1,5 +0,0 @@
-s = "hello world from Lua red 'red' \"red\" UredP"
--- for w in string.gmatch(s, "%pred%p") do
-for w in string.gmatch(s, "[\'\"]red[\"\']") do
-	print(w)
-end
