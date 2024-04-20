dd47fcc5e3f2f07b14047327e3f07afe11ace8e5
Jaehaks <wogkr3654@naver.com>
2024-04-20 17:40:17 +0900

    feat : add substitute.nvim
    This plugin makes it easy to yank and substitute contents

---
+++ lua/jaehak/plugins/substitute.lua
@@ -0,0 +1,16 @@
+return {
+	-- substitute contents automatically from default register 
+	'gbprod/substitute.nvim',
+	event = 'VeryLazy',
+	config = function ()
+		local sub = require('substitute')
+		sub.setup({
+			preserve_cursor_position = true,
+		})
+
+		vim.keymap.set("n", "s" , require('substitute').operator, { noremap = true , desc = 'substitute operator'})
+		vim.keymap.set("n", "ss", require('substitute').line    , { noremap = true , desc = 'substitute line'})
+		vim.keymap.set("n", "S" , require('substitute').eol     , { noremap = true , desc = 'substitute eok'})
+		vim.keymap.set("x", "s" , require('substitute').visual  , { noremap = true , desc = 'substitute visual'})
+	end
+}
