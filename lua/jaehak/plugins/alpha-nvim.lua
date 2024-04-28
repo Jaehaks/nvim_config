e7a4d663fd5b7389d72f47f8a03bfe8796c900c0
Jaehaks <wogkr3654@naver.com>
2024-04-28 22:30:20 +0900

    feat : use dashboard instead of alpha
    dashboard is faster loading time a little.
    footer() configuration is easy as I think.

--- lua/jaehak/plugins/alpha-nvim.lua
+++ lua/jaehak/plugins/alpha-nvim.lua
@@ -1,6 +1,8 @@
 return {
+{
 	'goolord/alpha-nvim',
---	enabled = false,
+	enabled = false,
+	event = 'VimEnter',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
 		local alpha = require('alpha')
@@ -13,11 +15,103 @@ return {
 			dashboard.button('d', '4 - Folder : D:\\MATLAB_Project', [[<Cmd>lua require("oil").open_float("D:\\MATLAB_Project")<CR>]]),                              -- open MATLAB Project
 			dashboard.button('p', '5 - Bookmarks'                  , [[<Cmd>lua require("grapple").toggle_tags()<CR>]]),                             -- open project list
 		}
-
-        alpha.setup(dashboard.config)	-- setting applyed 
+        alpha.setup(dashboard.config)	-- setting applied 
 
 		-- key mapping 
 		vim.keymap.set('n', '<leader>a', '<Cmd>Alpha<CR>', {noremap = true})
     end
+},
+{
+	-- dashboard is faster a little?
+	'nvimdev/dashboard-nvim',
+	-- enabled = false,
+	event = 'VimEnter',
+	init = function ()
+		-- vim.opt_local.modifiable = true
+		vim.opt.modifiable = true
+	end,
+	dependencies = { {'nvim-tree/nvim-web-devicons'}},
+	config = function()
+		require('dashboard').setup {
+			theme = 'doom',
+			hide = {
+				statusline = true,
+				tabline = true,
+				winbar = true
+			},
+			config = { -- doom configuration
+				center = {
+					{
+						icon = '',
+						desc = 'New File',
+						desc_hl = 'String',
+						key = 'e',
+						key_hl = 'Number',
+						key_format = '%s',
+						action = 'ene',
+					},
+					{
+						icon = '',
+						desc = 'Recent Files',
+						desc_hl = 'String',
+						key = 'r',
+						key_hl = 'Number',
+						key_format = '%s',
+						action = 'Telescope oldfiles',
+					},
+					{
+						icon = '',
+						desc = 'Folder : Config',
+						desc_hl = 'String',
+						key = 'c',
+						key_hl = 'Number',
+						key_format = '%s',
+						action = [[lua require("oil").open_float(vim.fn.stdpath("config") .. "/lua/jaehak/plugins")]],
+					},
+					{
+						icon = '',
+						desc = 'Folder : D:\\MATLAB_Project',
+						desc_hl = 'String',
+						key = 'd',
+						key_hl = 'Number',
+						key_format = '%s',
+						action = [[lua require("oil").open_float("D:\\MATLAB_Project")]],
+					},
+					{
+						icon = '',
+						desc = 'Bookmarks',
+						desc_hl = 'String',
+						key = 'p',
+						key_hl = 'Number',
+						key_format = '%s',
+						action = [[lua require("grapple").toggle_tags()]],
+					},
+				},
+				footer = function()
+					local stats = require("lazy").stats()
+					-- local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					local ms = stats.startuptime
+					-- return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+					return {
+						'Today : ' .. os.date('%Y-%m-%d, %a'),
+						'Startup Time : ' .. ms .. 'ms',
+						'Plugins : ' .. stats.loaded .. ' loaded / ' .. stats.count .. ' installed'
+					}
+				end,
+			}
+		}
+
+		if vim.o.filetype == "lazy" then
+			vim.cmd.close()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "DashboardLoaded",
+				callback = function()
+					require("lazy").show()
+				end,
+			})
+		end
+	end,
+
+}
 }
 
