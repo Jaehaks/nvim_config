d59808aee4a7b20b2c4f6b96fa0f569b5af4998c
Jaehaks <wogkr3654@naver.com>
2024-05-01 22:47:21 +0900

    key : add hover mode key

--- lua/jaehak/plugins/pantran.lua
+++ lua/jaehak/plugins/pantran.lua
@@ -35,10 +35,15 @@ return {
 		-- use replace mode : interactive mode makes background color noisy when I call Pantran in visual mode
 		local opts = {noremap = true, silent = true, expr = false}
 		vim.keymap.set({'n'}, '<leader>tw', ':Pantran source=ko target=en<CR>', vim.tbl_extend('keep',opts,{desc = 'show translate interactive window'}))
-		vim.keymap.set({'v'}, '<leader>te', ':Pantran source=ko target=en mode=replace<CR>', vim.tbl_extend('keep',opts,{desc = 'translate ko -> en'}))
-		vim.keymap.set({'v'}, '<leader>tk', ':Pantran source=en target=ko mode=replace<CR>', vim.tbl_extend('keep',opts,{desc = 'translate en -> ko'}))
+		vim.keymap.set({'v'}, '<leader>te', ':Pantran source=ko target=en mode=hover<CR>', vim.tbl_extend('keep',opts,{desc = 'hover ko -> en'}))
+		vim.keymap.set({'v'}, '<leader>tE', ':Pantran source=ko target=en mode=replace<CR>', vim.tbl_extend('keep',opts,{desc = 'replace ko -> en'}))
+		vim.keymap.set({'v'}, '<leader>tk', ':Pantran source=en target=ko mode=hover<CR>', vim.tbl_extend('keep',opts,{desc = 'hover en -> ko'}))
+		vim.keymap.set({'v'}, '<leader>tK', ':Pantran source=en target=ko mode=replace<CR>', vim.tbl_extend('keep',opts,{desc = 'replace en -> ko'}))
 	end
+},
 }
-}
--- 'uga-rosa/translate.nvim' : pantran.nvim is faster to translate 
+-- 'potamides/pantran.nvim' : using motion_translate or interactive mode, 'redraw' executes and it makes noisy background
+-- 'uga-rosa/translate.nvim' : pantran.nvim is faster to translate. but it supports floating window
+-- 'niuiic/translate.nvim' : Not windows support (need translate-shell)
+-- 'coffebar/crowtranslate.nvim' : Not windows support
 -- 한글과 영어를 교차해서 번역할 수 있는 플러그인을 추가했다 
