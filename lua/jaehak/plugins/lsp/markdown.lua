898d01fb0f6e1ccd2237aee8c106cfd230d4c6b8
Jaehaks <wogkr3654@naver.com>
2024-05-01 19:46:58 +0900

    feat : change markdown tools -> markdown.nvim
    1. delete peek.nvim configuration
        - use markdown-preview.nvim
    
    2. delete md-headers.nvim
        - it has too long loading time
        - use :MDToc in markdown.nvim
    
    3. disable headline.nvim
        - it is useful to identify heading line.
        - but it gives too much delay to load large markdown file
        - I'll disable this plugin until it supports disabling for
          large files, as I think it does.
    
    4. change mkdnflow.nvim => markdown.nvim
        - plugin for convenient workflow to write markdown.
        - mkdnflow is very functional, it seems to be well-used for creating tables and links.
          but for me, these are too much, so I disable lots of functions
    
    5. move autolist.nvim configuration to here.

--- lua/jaehak/plugins/lsp/markdown.lua
+++ lua/jaehak/plugins/lsp/markdown.lua
@@ -1,36 +1,5 @@
 return {
 {
-	-- markdown preview plugin
-	-- BUG: (app = 'webview' only) if file open directly with double click, peekopen does not work 
-	-- BUG: To execute peekopen function, you have to execute nvim-qt first and open the file lately
-	-- 
-	-- BUG: 240319 : open does not work in browser also 
-	-- 'toppair/peek.nvim',
-	-- enabled = false,
-	-- -- event = {'VeryLazy'},
-	-- ft = {'markdown'},
-	-- -- need "deno" => choco install deno 
-	-- -- build = 'deno task --quiet build:fast',
-	-- config = function ()
-	-- 	local peek = require('peek')
-	-- 	peek.setup({
-	-- 		auto_load = true, 	-- load preview when entering markdown buffer
-	-- 		close_on_bdelete = true,  -- close preview on buffer delete
-	-- 		app = 'browser',
-	-- 	})
-	--
-	-- 	-- make keymap
-	-- 	vim.keymap.set('n', '<leader>mp', function ()
-	-- 		if peek.is_open() then
-	-- 			return peek.close()
-	-- 		else
-	-- 			return peek.open()
-	-- 		end
-	-- 	end,
-	-- 	{desc = 'toggle markdown preview'})
-	-- end
-},
-{
 	-- more works better
 	'iamcco/markdown-preview.nvim',
 	ft = {'markdown'},
@@ -42,31 +11,12 @@ return {
 	end
 },
 {
-	-- show header list
-	'AntonVanAssche/md-headers.nvim',
-	version = '*',
-	lazy = false,
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'nvim-treesitter/nvim-treesitter'
-	},
-	config = function ()
-		local md_header = require('md-headers')
-		md_header.setup({
-			popup_auto_close = true
-
-		})
-
-		-- make keymap
-		vim.keymap.set('n','<leader>mh', '<Cmd>MarkdownHeaders<CR>', {silent = true, noremap = true, desc = 'open header list'})
-		vim.keymap.set('n','<leader>mc', '<Cmd>MarkdownHeadersClosest<CR>', {silent = true, noremap = true, desc = 'open header list closest'})
-	end
-
-},
-{
 	-- cursorline of headline highlight
+	-- BUG: I found that this headlines.nvim makes navigation slow in large markdown files (Changing setting depending on lien number does not support yet?)
+	-- it isn't bug. I think it's a simple problem caused by too many highlights.
+	-- it will be disabled temporarily
 	'lukas-reineke/headlines.nvim',
-	enabled = true,
+	enabled = false,
 	ft = {'markdown'},
 	dependencies = 'nvim-treesitter/nvim-treesitter',
 	config = function ()
@@ -93,5 +43,93 @@ return {
 			}
 		})
 	end
-}
+},
+{
+	-- surround emphasis / make links / TOC
+	'tadmccorkle/markdown.nvim',
+	ft = {'markdown'},
+	config = function ()
+		require('markdown').setup({
+			mappings = { -- applied to markdown file only
+				inline_surround_toggle      = "gs",
+				inline_surround_toggle_line = "gS",
+				inline_surround_delete      = "ds",
+				inline_surround_change      = "cs", -- `aaa` -> csci -> *aaa* 
+				link_add                    = "gm",
+				link_follow                 = "gM",
+				go_curr_heading             = "]c",
+				go_parent_heading           = "]p",
+				go_next_heading             = "]]",
+				go_prev_heading             = "[[",
+			},
+			inline_surround = {
+				emphasis      = { key = "i", txt = "*", },-- italic
+				strong        = { key = "b", txt = "**", },-- bold
+				strikethrough = { key = "s", txt = "~~", },-- cancel line
+				code          = { key = "c", txt = "`", },-- code line
+			},
+			link = {
+				paste = {
+					enable = true, -- make link when URLs are pasted
+				},
+			},
+			toc = {
+				omit_heading = "toc omit heading",
+				omit_section = "toc omit section",
+				markers = { "-" },
+			},
+			hooks = {
+				follow_link = nil,
+			},
+			on_attach = function (bufnr)
+				vim.keymap.set('n', '<leader>mh', '<Cmd>MDTocAll<CR>', {noremap = true, buffer = bufnr, desc = 'show contents list'})
+			end,
+		})
+	end
+},
+-- without any lsp, default filetype detector support syntax highlighting
+-- treesitter-markdown make using conceal possible. but it is not perfect
+	-- without headlines.nvim, It confused to discern code block and link => I turned of conceal of markdown
+-- mkdnflow.nvim : main function is link and table editing
+		-- 1) pros: it can access and make link with <CR>, it will be convenient reading README.md
+		-- 2) pros : increase / decrease heading 
+		-- 3) cons: it support auto indent numbering / but :MkdnUpdateNumbering cannot update like autolist.nvim 
+		--			=> use autolist.nvim and disable function about list item of mkdnflow 
+		-- 4) cons : conceal is not perfect. but treesitter cannot conceal without link 
+		-- mkdnflow is very functional, it seems to be well-used for creating tables and links.
+		-- but for me, these are too much, so I disable lots of functions
+--'AntonVanAssche/md-headers.nvim' : make toc list to navigate. it is replaced by markdown.nvim
+		-- it needs long load time
+{
+	-- add bullet automatically
+	-- BUG: if indent executed by TAB, the numbering does not change automatically, you should use :AutolistRecalculate
+	'gaoDean/autolist.nvim',
+	ft = {'markdown', 'text', 'NeogitCommitMessage'},
+	config = function ()
+		local autolist = require('autolist')
+		autolist.setup({
+
+		})
+
+		local User_markdown = vim.api.nvim_create_augroup('User_markdown', {clear = true})
+		vim.api.nvim_create_autocmd('FileType',{
+			group = User_markdown,
+			pattern = {'markdown', 'text', 'NeogitCommitMessage'},
+			callback = function ()
+				local opts = {noremap = true, buffer = 0}
+				vim.keymap.set('i', '<TAB>'     , '<Cmd>AutolistTab<CR>'             , opts)
+				vim.keymap.set('i', '<S-TAB>'   , '<Cmd>AutolistShiftTab<CR>'        , opts)
+				vim.keymap.set('i', '<CR>'      , '<CR><Cmd>AutolistNewBullet<CR>'   , opts)
+				vim.keymap.set('n', 'o'         , 'o<Cmd>AutolistNewBullet<CR>'      , opts)
+				vim.keymap.set('n', 'O'         , 'O<Cmd>AutolistNewBulletBefore<CR>', opts)
+				vim.keymap.set('n', '<leader>mn', '<Cmd>AutolistCycleNext<CR>'       , opts)
+				vim.keymap.set('n', '<leader>mr', '<Cmd>AutolistRecalculate<CR>'     , opts)
+				vim.keymap.set('n', 'dd'        , 'dd<Cmd>AutolistRecalculate<CR>'   , opts)
+				vim.keymap.set('v', 'd'         , 'd<Cmd>AutolistRecalculate<CR>'    , opts)
+			end
+		})
+
+	end
+},
+-- bullets-vim/bullets.nvim : it does not work in neovim
 }
--- lua/jaehak/plugins/lsp/nvim-treesitter.lua
+++ lua/jaehak/plugins/lsp/nvim-treesitter.lua
@@ -1,6 +1,5 @@
 return {
 {
-	enabled = true,
 	'nvim-treesitter/nvim-treesitter',
 	-- enabled = false,
 	lazy = true, -- by telescope
@@ -12,7 +11,7 @@ return {
 	config = function()
 		local configs = require('nvim-treesitter.configs')
 		local installs = require('nvim-treesitter.install')
-		local TShighlight = require('nvim-treesitter.highlight')
+		-- local TShighlight = require('nvim-treesitter.highlight')
 
 		installs.prefer_git = false
 		installs.compilers = {'gcc'}
@@ -23,13 +22,20 @@ return {
 								 'matlab',
 								 'python',
 							 	 'vim', 'vimdoc',
-								 'markdown', 'markdown_inline', 'html', 'json',
+								 'markdown', 'markdown_inline',
+								 'html', 'json',
 								 'diff', 'regex', 'ssh_config',
 							   	 'latex' -- for latex, tree-sitter-cli must be installed first, (scoop install main/tree-sitter)
 			},
 			highlight = {
 				enable = true,  -- if highlight, cannot use rainbow bracket. 
 								-- because treesitter managed highlighting bracket also. 
+				-- treesitter highlight decrease overall performance. 
+				-- highlight off if the file has too long lines, like help file lspconfig-server-configuration.md
+				-- it means that conceal of treesitter is disabled also.
+				disable = function (lang, bufnr)
+					return vim.api.nvim_buf_line_count(bufnr) > 5000
+				end,
 				additional_vim_regex_highlighting = false,
 			},
 			indent = {
@@ -38,34 +44,10 @@ return {
 			endwise = {			-- nvim-treesitter-endwise
 				enable = true
 			},
-			textobjects = { -- nvim-treesitter-textobjects
-				select = {
-					enable = true,
-					lookahead = true, -- if enter keymap, go to cursor the next treesitter textobject
-					keymaps ={
-						['ic'] = {query = '@comment.outer', desc = 'select inner part of comment region'}
-					},
-					selection_modes = {
-
-					},
-					include_surrounding_whitespace = false, -- text object is extended to include preceding white space
-					-- if true, unnecessary white space is included, 
-				}
-			}
 		})
 
-		-- custom_captures not work
+		-- custom_captures for latex not work
 		vim.api.nvim_set_hl(0, "@markup.math.latex" , { link = 'NightflyYello'}) -- it has bug. but is the best...
 	end,
 },
-{
-	-- add endwise, i think it would be useful when use language which does not support snippet
-	'RRethy/nvim-treesitter-endwise',
-	dependencies = {
-		'nvim-treesitter/nvim-treesitter'
-	},
-	config = function ()
-	end
-},
 }
--- tpope/vim-endwise : it doesn't work
