6696ee67cf0feae9f11eee9eb4e13bf8cf0276ff
Jaehaks <wogkr3654@naver.com>
2024-05-01 22:35:40 +0900

    add : snippets for markdown
    1) code block

---
+++ queries/LuaSnip/markdown.lua
@@ -0,0 +1,27 @@
+local ls     = require("luasnip")
+-- some shorthands...
+local s            = ls.snippet
+local t            = ls.text_node
+local i            = ls.insert_node
+
+
+local snippets, autosnippets = {}, {}
+
+-- 1) make code block
+local code_block = s({
+	trig = '``\\',
+	name = 'code block',
+	desc = 'make code block',
+	}, {
+	t({'```'}), i(1,'filetype'),
+	t({'','\t'}), i(2, '-- code'),
+	t({'','```'}),
+	t({'',''}), i(0,'')
+})
+table.insert(autosnippets, code_block)
+
+
+
+
+
+return snippets, autosnippets
