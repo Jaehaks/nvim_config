dab0e292d7d0e4f2f76ca7b2a9601b3440816d11
Jaehaks <wogkr3654@naver.com>
2024-05-01 22:41:55 +0900

    modified : use autosnippet
    change snippet structure from ls.add_snippets() to returning
    (snippet, autosnippet) tables
    
    add 3 snippets for function in lua

--- queries/LuaSnip/lua.lua
+++ queries/LuaSnip/lua.lua
@@ -1,158 +1,130 @@
-local ls = require("luasnip")
+local ls     = require("luasnip")
+local extras = require('luasnip.extras')
+local ex_fmt = require('luasnip.extras.fmt')
 -- some shorthands...
-local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local l = require("luasnip.extras").lambda
-local rep = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.conditions")
+local s            = ls.snippet
+local sn           = ls.snippet_node
+local t            = ls.text_node
+local i            = ls.insert_node
+local f            = ls.function_node
+local c            = ls.choice_node
+local d            = ls.dynamic_node
+local r            = ls.restore_node
+local l            = extras.lambda
+local rep          = extras.rep
+local p            = extras.partial
+local m            = extras.match
+local n            = extras.nonempty
+local dl           = extras.dynamic_lambda
+local fmt          = ex_fmt.fmt
+local fmta         = ex_fmt.fmta
+local types        = require("luasnip.util.types")
+local conds        = require("luasnip.extras.conditions")
 local conds_expand = require("luasnip.extras.conditions.expand")
 
 
-ls.add_snippets("lua", {
-	-- require module (duplicate lsp)
-	s({
-		trig = 'require(module)',
-		name = 'require',
-		desc = 'require lua module'
+local snippets, autosnippets = {}, {}
+local User_Luasnip = vim.api.nvim_create_augroup('User_Luasnip', {clear = true})
+local ft = '*.lua'
+
+-- make snippet work only out of comment region
+-- local CheckComment = function()
+-- 		local context = require('cmp.config.context')
+-- 		return not (context.in_treesitter_capture('comment') or context.in_syntax_group('Comment'))
+-- 	end
+
+-- [function] 1) empty
+local function_empty = s({
+	trig = 'fun@e',
+	name = 'funciton ()',
+	desc = 'make empty argument function',
+	-- condition = CheckComment
 	}, {
-		t("require(\'"), i(1, 'module'), t('\')')
-	}),
-	-- local variable declaration
-	s({
-		trig = 'local var',
-		name = 'local variable',
-		desc = 'make local variable with value'
-	},{
-		t('local '), i(1, 'variable'), t(' = '), i(2, 'value')
-	}),
-	-- local require declaration
-	s({
-		trig = 'local require',
-		name = 'local require',
-		desc = 'make local variable with require module'
-	},{
-		t('local '), i(1, 'variable'), t(' = require(\''), i(2, 'module'), t('\')')
-	}),
-	-- if statement
-	s({
-		trig = 'if',
-		name = 'if',
-		desc = 'if statement'
-	},{
-		t('if '), i(1, 'true'), t(' then'),
-		t({'','\t'}), i(2, '-- code'),
-		t({'', 'end'})
-	}),
-	-- basic for statement
-	s({
-		trig = 'for i=1:10',
-		name = 'for',
-		desc = 'basic for statement'
-	},{
-		t('for '), i(1, 'i'), t(' = '), i(2,'1'), t(' l :'), i(3,'10'), t(' then'),
-		t({'','\t'}), i(4, '-- code'),
-		t({'', 'end'})
-	}),
-	-- ipairs for statement
-	s({
-		trig = 'for ipairs',
-		name = 'for ipairs',
-		desc = 'ipair for statement'
-	},{
-		t('for '), i(1, 'i'), t(', '), i(2, 'v'), t(' in ipairs('), i(3,'table'), t(') do'),
-		t({'','\t'}), i(4, '-- code'),
-		t({'', 'end'})
-	}),
-	-- pairs for statement
-	s({
-		trig = 'for pairs',
-		name = 'for pairs',
-		desc = 'ipair for statement'
-	},{
-		t('for '), i(1, 'k'), t(', '), i(2, 'v'), t(' in pairs('), i(3,'table'), t(') do'),
-		t({'','\t'}), i(0, '-- code'),
-		t({'', 'end'})
-	}),
-	-- while statement
-	s({
-		trig = 'while',
-		name = 'while',
-		desc = 'while statement'
-	},{
-		t('while '), i(1, 'true'), t(' do'),
-		t({'','\t'}), i(2, '-- code'),
-		t({'', 'end'})
-	}),
-	-- function inline
-	s({
-		trig = 'function () inline',
-		name = 'function inline',
-		desc = 'make anonymous inline function'
-	},{
-		t('function('), i(1, ''), t(') '), i(2, 'code'), t(' end'),
-	}),
-	-- -- function comment
-	-- s({
-	-- 	trig = 'function () comment',
-	-- 	name = 'function comment',
-	-- 	desc = 'make function with comment'
-	-- },{
-	-- 	t('-- [[ '), i(1, 'one-line-summary'), t(']]'),
-	-- 	t({'', '-- @param '}), i(1,'name'), t(' - '), i(1,'type'), t(' : '), i(1,'description'),
-	-- 	t({'', '-- @return '}), i(1,'name'), t(' - '), i(1,'type'), t(' : '), i(1,'description'),
-	-- 	t({'','local '}), i(1,' vname'), t(' = function '), i(2, 'fname'), t('('), i(3, 'param'), t(')'),
-	-- 	t({'','\t'}), i(4, '-- code'),
-	-- 	t({'', 'end'})
-	-- }),
+	t({'function ('}), i(1), t({')'}),
+	t({'','\t'}), i(2, '-- code'),
+	t({'', 'end'}),
+	t({'',''}), i(0,'')
 })
+table.insert(autosnippets, function_empty)
+
+-- [function] 2) empty inline
+local function_empty_inline = s({
+	trig = 'fun@i',
+	name = 'funciton() end',
+	desc = 'make empty argument inline function',
+	-- condition = CheckComment
+	}, {
+	t({'function() '}),  i(1, '-- code'), t({' end'}),
+	t({'',''}), i(0,'')
+})
+table.insert(autosnippets, function_empty_inline)
 
 
-local function add_param(args)
-	local param_str = args[1][1]
-	param_str = param_str:gsub(' ','') 	-- remove blank
-	if param_str == '' then return '' end -- if there is no param, return
 
-	local params = vim.split(param_str, ',') -- make table after spliting parameter
-	local doc_comments = {''}
-	for _, param in ipairs(params) do -- add param string to doc_comments table
-		table.insert(
-			doc_comments,
-			string.format('-- @param %s - type : description', param)
-		)
-	end
-
-	return doc_comments
-end
-
-local func_template = [==[
--- [[@function : {}]]{}
-function {}({})
+-- [function] 3) with comment
+local template_snip = [[
+-- {}{} 
+local {} = function ({})
 	{}
 end
-]==]
+{}
+]]
 
--- https://www.youtube.com/watch?v=xxNZoFk7jtw
--- it has half funciton, it returns the comment after all parameters are entered, 
--- need realtime comment
-ls.add_snippets('lua', {
-	s('function () comment',
-		fmt(func_template, {
-			i(1, 'one-line-summary'),
-			f(add_param, {3}),
-			i(2, 'fname'), i(3, 'params'),
-			i(4, '-- code')
-		}))
-})
+local function_comment = s({
+	trig = 'fun@c',
+	name = 'full funciton()',
+	desc = 'make full argument function and comment',
+	},
+	fmt(template_snip, {
+		i(5, {'Desciption'}),
+		d(4, function(args)
+			local param_str = args[1][1] -- get all param string
+			param_str = param_str:gsub(' ', '') -- delete space
+			if param_str == '' then
+				return sn(1, {t('')})	-- if there are no param, don't show comment about it 
+			end
+			local params = vim.split(param_str, ',') -- make param_str to table with separator
+			local nodes = {}
+			for index, param in ipairs(params) do
+				table.insert(
+					nodes,
+					sn(
+						index,
+						fmt('\n\n-- @param {} <{}> : {}', { -- I don't know why \n must be duplicated to make new line
+							t(param),
+							i(1, 'type'),
+							i(2, 'description')
+						})
+					)
+				)
+			end
+			return sn(nil, nodes)
+		end, {2}),
+		i(1, 'name'),
+		i(2),
+		i(3, '-- code'),
+		i(0)
+	})
+)
+table.insert(autosnippets, function_comment)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return snippets, autosnippets
+
+-- cautions!)
+-- 1) if fmt is used as the second argument, you shouldn't surround it with braces 
