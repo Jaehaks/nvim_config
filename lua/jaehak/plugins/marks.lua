9d0a69aadd4550357c9532a16c503de84a4ea0fa
Jaehaks <wogkr3654@naver.com>
2024-05-06 00:17:29 +0900

    comment : notify a bug (cannot delete marks)

--- lua/jaehak/plugins/marks.lua
+++ lua/jaehak/plugins/marks.lua
@@ -3,6 +3,14 @@ return {
 	'chentoast/marks.nvim',
 	config = function ()
 		require('marks').setup({
+			default_mappings = true,
+			sign = true, -- show marks name in signcolumn
+			-- force_write_shada = true -- deleting global marks update the shada file 
+				-- BUG: neovim has an issue that it cannot delete local marks
+				-- setting 'force_write_shada' to true, `dm[a-z]` will works but `dm<space>` not works. 
+				-- set this flag will clear the shada file very often... 
+
+
 
 		})
 	end
