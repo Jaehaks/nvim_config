51af05483855ed3108577e5aa795d2069d3c21db
Jaehaks <wogkr3654@naver.com>
2024-05-23 23:45:16 +0900

    feat : add tex snippets
    1) \begin trigger
    2) \left - \right
    3) \frac
    4) bmatrix
    5) vector

--- queries/LuaSnip/tex.lua
+++ queries/LuaSnip/tex.lua
@@ -31,11 +31,10 @@ local temp_fmt = [[
 \begin{{{}}}
 	{}
 \end{{{}}}
-{}
 ]]
 
 local begin_end = s({
-	trig = '\\begin',
+	trig = '\\beg',
 	name = 'begin ~ end',
 	desc = 'begin ~ end',
 	},
@@ -45,19 +44,114 @@ local begin_end = s({
 		f(function(args)
 			return args[1][1]
 		end,{1}),
-		i(0),
 	})
 )
 table.insert(autosnippets, begin_end)
 
 
+-- [equation] 1) left - right
+temp_fmt = [[
+\left{} {} \right{}
+]]
+
+local left_right = s({
+	trig = '\\lef',
+	name = 'left ~ right',
+	desc = 'left ~ right',
+	},
+	fmt(temp_fmt, {
+		i(1),
+		i(2),
+		f(function (args)
+			local str = args[1][1]
+			local delim = str:sub(1,1) -- get first char
+			local char1 = ''
+			local out = ''
+
+			-- check first char is '\'
+			if delim == '\\' then
+				delim = str:sub(2,2)	-- get second char
+				char1 = '\\'
+			end
+
+			-- add right symbol
+			if delim == '(' then out = ')'
+			elseif delim == '{' then out = '}'
+			elseif delim == '[' then out = ']'
+			elseif delim == '<' then out = '>'
+			else out = ''
+			end
+
+			return char1 .. out
+		end,{1}),
+	})
+)
+table.insert(autosnippets, left_right)
+
+
+-- [equation] 2) fractional number
+temp_fmt = [[
+\frac{{{}}}{{{}}}
+]]
+
+local frac = s({
+	trig = '\\fra',
+	name = 'frac{}{}',
+	desc = 'frac{}{}',
+	},
+	fmt(temp_fmt, {
+		i(1),
+		i(2),
+	})
+)
+table.insert(autosnippets, frac)
 
 
 
 
+-- [equation] 3) bmatrix 2x2
+temp_fmt = [[
+\begin{{bmatrix}}
+	{} & {} \\
+	{} & {} \\
+\end{{bmatrix}}
+]]
+
+local mat = s({
+	trig = '\\mat',
+	name = 'bmatrix 2x2',
+	desc = 'bmatrix 2x2',
+	},
+	fmt(temp_fmt, {
+		i(1),
+		i(2),
+		i(3),
+		i(4),
+	})
+)
+table.insert(autosnippets, mat)
 
 
 
+-- [equation] 4) vector 2x1
+temp_fmt = [[
+\begin{{bmatrix}}
+	{} \\
+	{} \\
+\end{{bmatrix}}
+]]
+
+local vect = s({
+	trig = '\\vec',
+	name = 'vector 2x1',
+	desc = 'vector 2x1',
+	},
+	fmt(temp_fmt, {
+		i(1),
+		i(2),
+	})
+)
+table.insert(autosnippets, vect)
 
 
 
