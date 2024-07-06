4488171214f3a79d99932edf6fee733d8afd172f
Jaehaks <wogkr3654@naver.com>
2024-07-06 10:29:49 +0900

    fix(neogit) : use recent version with neovim v0.10.0
    Now, neogit stabilized as I think. the slow response issue is removed.
    docs of neogit don't updated yet. you must see docs in github
    
    1) delete telescope dependencies, we do not use it
    2) commit_editor shows diff default. => disable
    3) neogit loading time is longer than before, so if `console_timeout` is
       default value (2000), neogit shows failure message.
       so make wait time longer
    4) default moving key is reset. so I change the keys

--- lua/jaehak/plugins/neogit.lua
+++ lua/jaehak/plugins/neogit.lua
@@ -1,15 +1,12 @@
 return {
 	"NeogitOrg/neogit",
-	version = 'v0.0.1', -- neovim 0.9.5 compatible
+	-- version = 'v0.0.1', -- neovim 0.9.5 compatible
 	keys = { -- negit loading time is too long
 		{'<leader>go', '<Cmd>Neogit<CR>', desc = 'open neogit default', 'n'}
 	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",         -- required
 		"sindrets/diffview.nvim",        -- optional - Diff integration
-
-		-- Only one of these is needed, not both.
-		"nvim-telescope/telescope.nvim", -- optional
 	},
 	config = function ()
 		local neogit = require('neogit')
@@ -20,20 +17,23 @@ return {
 			status = {
 				recent_commit_count = 20,
 			},
-			-- commit_editor = {
-			-- 	kind = 'split_above' -- horizontal split
-			-- },
-			-- popup = {
-			-- 	kind = 'split_above'
-			-- },
+			commit_editor = {
+				kind = 'split', -- horizontal split below
+				show_staged_diff = false,	-- disable diff in commit message 
+			},
+			console_timeout = 10000,	-- neogit loading slow.
 			integrations = {
 				telescope = false,
 				diffview = true,
 				fzf_lua = false,
-			}
+			},
+			mappings = {
+				status = {
+					['k'] = 'MoveDown',
+					['j'] = 'MoveUp',
+				}
+			},
 		})
-		-- it is very wiere
-		-- test tset it is just test 
 
 		-- vim.keymap.set('n', '<leader>go', neogit.open, {desc = 'open neogit default'})
 	end
