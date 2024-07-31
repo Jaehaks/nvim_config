720eac3e87598e3838312679aac59cb7aaed9873
Jaehaks <wogkr3654@naver.com>
2024-07-31 22:02:23 +0900

    fix(autoclose) : change condition of ' and `
    1) I use matlab. and have to use ' for transpose of matrix
        when I insert ' behind of [a b c], '' inserted.
    
        so if ' is input after ] and ', it makes autoclose doesn't work
    
    2) I use markdown and it makes code block with triple `
        but when I enter ` three times, it makes one another backtick
        I fixed this .

--- lua/jaehak/plugins/lsp/autolcose.lua
+++ lua/jaehak/plugins/lsp/autolcose.lua
@@ -22,8 +22,8 @@ return {
 				['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
 
 				['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
-				["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
-				['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+				["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\%]\'].', register = { cr = false } },
+				['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\%`].', register = { cr = false } },
 			},
 		})
 	end
