a64ca78187fc3b19929a077fc907fe45838a54ad
Jaehaks <wogkr3654@naver.com>
2024-08-01 22:44:19 +0900

    fix(whichkey) : rewrite config according to new version of which-key

--- lua/jaehak/plugins/which-key.lua
+++ lua/jaehak/plugins/which-key.lua
@@ -17,57 +17,23 @@ return {
 					z = false,
 				}
 			},
-			triggers_blacklist = {
-				i = {'j', 'k', 'f'} -- don't show which key panel when you type this list in 'i' mode
-			}
 		})
 
-		-- manually register's opts
-		local nopts = {
-		  mode = "n", -- NORMAL mode
-		  prefix = "<leader>",
-		  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-		  silent = true, -- use `silent` when creating keymaps
-		  noremap = true, -- use `noremap` when creating keymaps
-		  nowait = true, -- use `nowait` when creating keymaps
-		}
+		wk.add({
+			{'<leader>c', group = 'Color', mode = 'n'},
+			{'<leader>e', group = 'Oil explorer', mode = 'n'},
+			{'<leader>f', group = 'Telescope', mode = 'n'},
+			{'<leader>x', group = 'Trouble', mode = 'n'},
+			{'<leader>l', group = 'Lspsaga', mode = 'n'},
+			{'<leader>m', group = 'Markdown', mode = 'n'},
+			{'<leader>h', group = 'Gitsign', mode = 'n'},
+			{'<leader>g', group = 'Neogit', mode = 'n'},
+			{'<leader>p', group = 'Project', mode = 'n'},
+			{'<leader>s', group = 'Nvim-spectre', mode = {'n', 'v'}},
+			{'<leader>t', group = 'Terminal / Translate', mode = 'n'},
+			{'<leader>t', group = 'Translate', mode = 'v'},
+			{'<leader>w', group = 'Formatter', mode = {'n', 'v'}},
+		})
 
-
-		-- mapping 
-		local nmappings = {
-			c = { name = 'Color' },
-			e = { name = 'Oil explorer' },
-			f = { name = 'Telescope' },
-			x = { name = 'Trouble' },
-			l = { name = 'Lspsaga' },
-			m = { name = 'Markdown' },
-			h = { name = 'Gitsign' },
-			g = { name = 'Neogit' },
-			p = { name = 'Project'},
-			s = { name = 'Nvim-spectre'},
-			t = { name = 'Terminal / Translate'},
-			w = { name = 'Formatter'},
-		}
-
-		-- manually register's opts
-		local vopts = {
-		  mode = "v", -- VISUAL mode
-		  prefix = "<leader>",
-		  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-		  silent = true, -- use `silent` when creating keymaps
-		  noremap = true, -- use `noremap` when creating keymaps
-		  nowait = true, -- use `nowait` when creating keymaps
-		}
-
-
-		-- mapping 
-		local vmappings = {
-			w = { name = 'Formatter' },
-			t = { name = 'Translate' },
-			s = { name = 'Nvim-spectre' },
-		}
-
-		wk.register(nmappings, nopts)	-- register mappings
-		wk.register(vmappings, vopts)	-- register mappings
 	end
 }
