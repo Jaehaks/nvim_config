b7e98b8676564daca5f3457bd37ed5810cebd06f
Jaehaks <wogkr3654@naver.com>
2024-10-05 10:03:53 +0900

    feat(autocmd) : add matlab_ls to terminate lsp list

--- lua/jaehak/core/autocmds.lua
+++ lua/jaehak/core/autocmds.lua
@@ -39,12 +39,43 @@ vim.api.nvim_create_autocmd({"LspAttach"}, {
 		local client = vim.lsp.get_client_by_id(client_id)
 
 		if client then
+			-- insert recent harper-ls
 			if client.name == 'harper_ls' then
-				table.insert(delete_process_list, GetProcessId('harper-ls.exe')[1])
+				local harper_ls_tbl = GetProcessId('harper-ls.exe')
+				table.insert(delete_process_list, harper_ls_tbl[1])
+			end
+
+			-- insert recent matlab-ls
+			-- matlab lsp executes two process matlab.exe and MATLAB.exe
+			-- and the process run some times after lsp is executes
+			if client.name == 'matlab_ls' then
+				local attempts = 0
+				local max_attempts = 5
+				local timer = vim.loop.new_timer()
+				local matlab_tbl = GetProcessId('matlab.exe')
+				timer:start(100, 1000, vim.schedule_wrap(function()
+					attempts = attempts + 1
+					if next(matlab_tbl) or attempts >= max_attempts then
+						timer:stop()
+						table.insert(delete_process_list, matlab_tbl[1])
+						table.insert(delete_process_list, matlab_tbl[2])
+						if attempts >= max_attempts then
+							print('over timer')
+						end
+					end
+					matlab_tbl = GetProcessId('matlab.exe')
+				end))
+
 			end
 		end
 	end
 })
+
+vim.api.nvim_create_user_command("GetProcessId", function (opts)
+	local tbl = GetProcessId('matlab.exe')
+	vim.print(tbl)
+end, {nargs = 1})
+
 
 -- clear some procedure after VimLeave
 vim.api.nvim_create_autocmd({"VimLeave"}, {
@@ -77,6 +108,24 @@ vim.api.nvim_create_autocmd({"BufReadPre", "BufNewFile"}, {
 	once = true,
 })
 
+-- auto stop lsp when all buffers related the lsp are deleted
+-- vim.api.nvim_create_autocmd("BufDelete", {
+-- 	callback = function(args)
+-- 		print('buf delete')
+-- 		local bufnr = args.buf
+-- 		local clients = vim.lsp.get_active_clients({bufnr = bufnr})
+-- 		for _, client in ipairs(clients) do
+-- 			-- print(client.id)
+-- 			print(#client.attached_buffers)
+-- 			-- Check if the client is only attached to this buffer
+-- 			if #client.attached_buffers == 0 then
+-- 				-- vim.lsp.stop_client(client.id)
+-- 				vim.cmd('LspStop ' .. client.id)
+-- 				print(string.format("Stopped LSP client %s", client.name))
+-- 			end
+-- 		end
+-- 	end,
+-- })
 
 ------------ Auto Trim white space at the end of line --------------
 local aug_TrimWhiteSpace = vim.api.nvim_create_augroup("aug_TrimWhiteSpace", { clear = true })
