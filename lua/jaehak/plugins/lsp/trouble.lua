c2933a4dc1ac75d2c058daba9d7451e347561ee0
Jaehaks <wogkr3654@naver.com>
2024-10-20 17:17:44 +0900

    fix(trouble) : fix options and keys following new version

--- lua/jaehak/plugins/lsp/trouble.lua
+++ lua/jaehak/plugins/lsp/trouble.lua
@@ -2,16 +2,32 @@ return {
 	'folke/trouble.nvim',
 	event = 'LspAttach',
 	dependencies = {'nvim-tree/nvim-web-devicons'},
-	opts = {},
 	config = function ()
 		local trouble = require('trouble')
 		trouble.setup({
-			action_keys = {
-				next = 'k',			-- change move key
-				previous = 'j',		-- change move key 
+			auto_close   = true,
+			auto_preview = true, -- move view to the selected item automatically
+			focus        = true, -- focus trouble window when opened
+			follow       = true, -- start trouble item from current cursorline
+
+			keys = {
+				["?"]     = "help",
+				q         = "close",
+				o         = "jump_close",
+				["<esc>"] = "cancel",
+				["<cr>"]  = "jump",
+				["<c-s>"] = "jump_split",
+				["<c-v>"] = "jump_vsplit",
+				-- go down to next item (accepts count)
+				k         = "next",
+				-- go up to prev item (accepts count)
+				j         = "prev",
+				dd        = "delete",
+				d         = { action = "delete", mode = "v" },
+				i         = "inspect",
 			},
---			auto_open = true,
 		})
-		vim.keymap.set({'n'}, '<leader>xt', function() trouble.toggle('document_diagnostics') end)
+		vim.keymap.set({'n'}, '<leader>xt', '<Cmd>Trouble diagnostics toggle filter.buf=0<CR>', {desc = 'Toggle Trouble window of current buffer'})
+		vim.keymap.set({'n'}, '<leader>xT', '<Cmd>Trouble diagnostics toggle <CR>', {desc = 'Toggle Trouble window of project'})
 	end
 }
