333fd03f63e47f6609fcacd0a0118df13efd6464
Jaehaks <wogkr3654@naver.com>
2024-10-22 00:39:40 +0900

    style(marks) : remove blank lines

--- lua/jaehak/plugins/marks.lua
+++ lua/jaehak/plugins/marks.lua
@@ -5,13 +5,10 @@ return {
 		require('marks').setup({
 			default_mappings = true,
 			sign = true, -- show marks name in signcolumn
-			-- force_write_shada = true -- deleting global marks update the shada file 
+			-- force_write_shada = true -- deleting global marks update the shada file
 				-- BUG: neovim has an issue that it cannot delete local marks
-				-- setting 'force_write_shada' to true, `dm[a-z]` will works but `dm<space>` not works. 
-				-- set this flag will clear the shada file very often... 
-
-
-
+				-- setting 'force_write_shada' to true, `dm[a-z]` will works but `dm<space>` not works.
+				-- set this flag will clear the shada file very often...
 		})
 	end
 }
