4155763abebbf31dfc06b89e32abe60515f23479
Jaehaks <wogkr3654@naver.com>
2024-10-22 00:38:39 +0900

    style(hlchunk) : delete unused plugin - mini-indent
    hlchunk replaces mini-indentation plugin entirely

--- lua/jaehak/plugins/hlchunk.lua
+++ lua/jaehak/plugins/hlchunk.lua
@@ -1,30 +1,5 @@
 return {{
-	-- support current indentation only(good), but if there are code in indentline, the line will be  cut
-	'echasnovski/mini.indentscope',
-	enabled = false,
-	version = '*',
-	config = function()
-		local indents = require('mini.indentscope')
-		indents.setup({
-			draw = {
-				delay = 0,
-				animation = indents.gen_animation.none(),	-- function return 0
-			},
-		})
-
-		-- Disable for certain filetypes
-		vim.api.nvim_create_autocmd({'FileType'},{
-			desc = 'disable indentscope for certain filetypes',
-			callback = function()
-				local ignore_filetype = { 'help', 'lazy', 'TelescopePrompt', 'mason', 'Trouble', 'dashboard', 'Oil' , 'NeogitStatus', 'grapple'}
-				if vim.tbl_contains(ignore_filetype, vim.bo.filetype) then
-					vim.b[0].miniindentscope_disable = true		-- if use b (buffer) variable,   set b[buf_id]
-				end
-			end,
-		})
-	end
-},
-{
+	-- show region of indentation block
 	"shellRaining/hlchunk.nvim",
 	enabled = true,
 	config = function()
@@ -61,3 +36,4 @@ return {{
 }
 --	'lukas-reineke/indent-blankline.nvim' 		  -->>>> works well, not support current indentation level, it depends on treesitter
 --	'nvimdev/indentmini.nvim' : loading time is fast, but not work
+--  'echasnovski/mini.indentscope' : I think it is slower than hlchunk. and hlchunk's graphic is more good
