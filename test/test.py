401cc12eb76627835afd38552cb28d0305fd9cfa
Jaehaks <wogkr3654@naver.com>
2025-01-12 20:45:59 +0900

    fix(test) : add test for python

--- test/test.py
+++ test/test.py
@@ -17,10 +17,32 @@ def countdown(t: int):
         print(os.getcwd())
 
 
-# t = input('Enter the time in seconds')
+print()
+t = input('Enter the time in seconds')
 # t = 'est'
 t = 300
+
+# create a class
+class Room:
+    length = 0.0
+    breadth = 0.0
+
+    # method to calculate area
+    def calculate_area(self):
+        print("Area of Room =", self.length * self.breadth)
+
+# create object of Room class
+study_room = Room(r)
+
+# assign values to all the properties
+study_room.length = 42.5
+study_room.breadth = 30.8
+
+# access method inside class
+study_room.calculate_area()
 
 
 countdown(t)
 count('test')
+
+
