399d9eacfbe0241849ecff6a62e3eba5196270e9
Jaehaks <wogkr3654@naver.com>
2025-01-28 11:50:48 +0900

    feat(split) : add split.nvim to split with punctuation

---
+++ lua/jaehak/plugins/split.lua
@@ -0,0 +1,54 @@
+return {
+	"wurli/split.nvim",
+	opts = {
+		keymaps = {
+			-- interactive : after keymap input, dialog opens to get which character is criteria
+			-- 				 'gsi' + ',' (, is criteria to split)
+			-- operator_pending : after keymap input, wait to recognize what is the range to apply split
+			-- 				 'gsi' + 'i]' + ','(if interactive), it splits with ',' in []
+			-- 				 if it is false, it split all ',' in plain text out of braces
+			-- break_placement : where the split character is located after split
+			-- 				'before_pattern' : ',' is located before word
+			-- 				'after_pattern'  : ',' is located after word
+			-- samrt_ignore : as default, if there are both code and comment, the split is applied for code
+			["gsf"] = {
+                interactive = true,
+				operator_pending = true,
+				break_placement = 'after_pattern',
+			},
+			["gsb"] = {
+                interactive = true,
+				operator_pending = true,
+				break_placement = 'before_pattern',
+			},
+			["gsF"] = {
+				interactive = true,
+				operator_pending = false,
+				break_placement = 'after_pattern',
+			},
+			["gsB"] = {
+				interactive = true,
+				operator_pending = false,
+				break_placement = 'before_pattern',
+			},
+		},
+        interactive_options = {
+            [","] = ",",
+            [";"] = ";",
+            [" "] = "%s+",       -- white space more than 1
+            ["-"] = "[%+%-%*/]", -- arithmetic operators
+            ["<"] = {            -- all inequality like <, <=, >=
+                pattern = "[<>=]=?",
+                break_placement = "before_pattern"
+            },
+            ["."] = {            -- all period of sentence (.?!)
+                pattern = "[%.?!]%s+",
+                unsplitter = " ",
+                smart_ignore = "code",
+                quote_characters = {},
+                brace_characters = {}
+            }
+        },
+		set_default_mappings = false,
+	},
+}
