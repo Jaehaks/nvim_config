9c0a9954363a96ab7d67578bd3de2b409dcdc5be
Jaehaks <wogkr3654@naver.com>
2025-01-29 20:56:38 +0900

    fix(colorizer) : add nvim-colorizer to show colorcode

--- lua/jaehak/plugins/colorcode.lua
+++ lua/jaehak/plugins/colorcode.lua
@@ -1,7 +1,7 @@
 return {
 {
 	'brenoprata10/nvim-highlight-colors',
-	enabled = true,
+	enabled = false,
 	init = function ()
 		vim.opt.termguicolors = true
 	end,
@@ -36,6 +36,41 @@ return {
 	end
 },
 {
+	enabled = true,
+    "catgoose/nvim-colorizer.lua",
+	branch = 'fix/141-quotes_and_doublequotes',
+    event = "BufReadPre",
+	config = function ()
+		local colorizer = require('colorizer')
+		colorizer.setup({
+			-- lazy_load = true,
+			user_default_options = {
+				names_custom = {
+					["'r'"] = "#FF0000",
+					['"r"'] = "#FF0000",
+					["'g'"] = "#00FF00",
+					['"g"'] = "#00FF00",
+					["'b'"] = "#0000FF",
+					['"b"'] = "#0000FF",
+					["'c'"] = "#00FFFF",
+					['"c"'] = "#00FFFF",
+					["'m'"] = "#FF00FF",
+					['"m"'] = "#FF00FF",
+					["'y'"] = "#FFFF00",
+					['"y"'] = "#FFFF00",
+					["'k'"] = "#000000",
+					['"k"'] = "#000000",
+					["'w'"] = "#FFFFFF",
+					['"w"'] = "#FFFFFF",
+				}
+			}
+
+		})
+
+		vim.keymap.set('n','<leader>cc', '<Cmd>ColorizerToggle<CR>', {desc = 'Show color code toggle'})
+	end
+},
+{
 	-- graphical color picker
 	'eero-lehtinen/oklch-color-picker.nvim',
 	config = function()
@@ -54,6 +89,8 @@ return {
 }
 
 -- brenoprata10/nvim-highlight-colors : labels for custom_colors are just string, not regex
+-- 						it process for full-file highlighting. so it would be slow on large files.
+-- 						it is what it migrate to nvim-colorizer
 -- chrisbra/colorizer : it works very well. but I just want lua plugin
 -- uga-rosa/ccc.nvim : cannot show colors properly Since What I install other plugins. I think it is bug..
 -- 					   if modify some colorscheme, palette lose colors
