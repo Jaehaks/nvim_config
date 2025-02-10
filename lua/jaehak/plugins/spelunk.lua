b887bdba59302bd8d297acc66a159f4ecf80c325
Jaehaks <wogkr3654@naver.com>
2025-02-10 21:57:25 +0900

    fix : disable `spelunk` and `telescope` temporarily
    spelunk and telescope has very long startup time to use.
    
    telescope will be replaced by snacks which is more fast startup
    
    spelunk has long loading time but I don't use it that often yet.
    I'll use it when I feed the need.

--- lua/jaehak/plugins/spelunk.lua
+++ lua/jaehak/plugins/spelunk.lua
@@ -1,6 +1,7 @@
 return {
 	-- persistent mark manager
 	'EvWilson/spelunk.nvim',
+	enabled = false, -- add when I use it
 	dependencies = {
 		'nvim-lua/plenary.nvim',         -- For window drawing utilities
 		'nvim-telescope/telescope.nvim', -- Optional: for fuzzy search capabilities
