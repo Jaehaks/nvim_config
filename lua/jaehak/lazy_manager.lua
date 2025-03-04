f3a69d3ddb00833bb492168c3bd0c3eaa656ac90
Jaehaks <wogkr3654@naver.com>
2025-03-04 21:44:32 +0900

    fix(lazy) : replace vim.loop with vim.uv

--- lua/jaehak/lazy_manager.lua
+++ lua/jaehak/lazy_manager.lua
@@ -3,7 +3,7 @@ if vim.g.has_win32 == 1 then
 	lazypath = lazypath:gsub('/', '\\')
 end
 
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
