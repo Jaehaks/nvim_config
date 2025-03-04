aa48fb791e21ca91f19c8b9a3a0631fab361b7a2
Jaehaks <wogkr3654@naver.com>
2025-03-04 21:06:52 +0900

    fix(lazy) : add auxiliary.lua to lazy.nvim
    I forgot to add auxiliary.lua to lazy.nvim import list.
    nvim-web-devicons and plenary.nvim cannot be loaded properly until now

--- lua/jaehak/lazy_manager.lua
+++ lua/jaehak/lazy_manager.lua
@@ -21,6 +21,7 @@ vim.opt.rtp:prepend(lazypath)
 -------------------------------------------------------------
 if vim.g.vscode == nil then
 	require('lazy').setup({
+		{import = 'jaehak.plugins.auxiliary'     	  , enabled = true},
 		{import = 'jaehak.plugins.buffer_manager'     , enabled = false},
 		{import = 'jaehak.plugins.colorscheme'        , enabled = true},
 		{import = 'jaehak.plugins.snacks'             , enabled = true},
