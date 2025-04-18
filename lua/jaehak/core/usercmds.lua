b5166c1eacaf82663be0e15849ca6e6d3e365c73
Jaehaks <wogkr3654@naver.com>
2025-04-18 22:58:49 +0900

    feat(usercmds) : add `:Tabgit` command to change indent for git style
    Some github repositories use 2 as indent or tabstop but i don't.
    To make PR and issues convenient, It needs to change tabstop easily.
    
    So I made my own command :Tabgit to toggle mode tabstop 2 and 4

--- lua/jaehak/core/usercmds.lua
+++ lua/jaehak/core/usercmds.lua
@@ -19,3 +19,34 @@ end, {nargs = 1})
 vim.api.nvim_create_user_command("LspInfo", function (opts)
 	vim.cmd('checkhealth vim.lsp')
 end, {desc = 'LspInfo'})
+
+
+local tab_default = {
+	tabstop     = vim.api.nvim_get_option_value('tabstop', {}),
+	softtabstop = vim.api.nvim_get_option_value('softtabstop', {}),
+	shiftwidth  = vim.api.nvim_get_option_value('shiftwidth', {}),
+	expandtab   = vim.api.nvim_get_option_value('expandtab', {}),
+}
+
+---------- Editing ------------
+vim.api.nvim_create_user_command("Tabgit", function (opts)
+	if vim.api.nvim_get_option_value('tabstop', {}) == tab_default.tabstop then
+		vim.opt.tabstop     = 2
+		vim.opt.softtabstop = 2
+		vim.opt.shiftwidth  = 2
+		vim.opt.expandtab   = true
+	else
+		vim.opt.tabstop     = tab_default.tabstop
+		vim.opt.softtabstop = tab_default.softtabstop
+		vim.opt.shiftwidth  = tab_default.shiftwidth
+		vim.opt.expandtab   = tab_default.expandtab
+	end
+
+	if opts.range == 2 then
+		vim.cmd('normal! gv=')
+	else
+		local pos = vim.api.nvim_win_get_cursor(0)
+		vim.cmd('normal! gg=G')
+		vim.api.nvim_win_set_cursor(0, pos)
+	end
+end, {desc = 'Toggle to make tab size 2'})
