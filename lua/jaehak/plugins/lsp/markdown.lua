8de88612821797468caa922a989986f2911ba628
Jaehaks <wogkr3654@naver.com>
2025-06-11 11:36:38 +0900

    fix(markview) : match heading color in insert mode with normal mode
    After some update of markview, it changes some color set about heading.
    It broke the relationship between insert mode and normal mode heading.

--- lua/jaehak/plugins/lsp/markdown.lua
+++ lua/jaehak/plugins/lsp/markdown.lua
@@ -29,11 +29,11 @@ return {
 		vim.api.nvim_set_hl(0, "MarkviewListItemMinus", {fg = '#F68C6B'} )
 		vim.api.nvim_set_hl(0, "MarkviewListItemPlus" , {fg = '#b1c61c'} )
 		vim.api.nvim_set_hl(0, "MarkviewListItemStar" , {fg = '#00d1dd'} )
-		vim.api.nvim_set_hl(0, 'MarkviewHeading5'     , {fg = '#F0FF7C',  bg = '#2D2F1D'})
 		vim.api.nvim_set_hl(0, 'MarkviewCheckboxCancelled', {fg = '#999999'})
 		vim.api.nvim_set_hl(0, 'MarkviewBlockQuoteDefault', {link = 'Normal'}) -- default block quote color
 		vim.api.nvim_set_hl(0, 'MarkviewBlockQuoteAnswer', {default = true, fg = '#FE86D8'}) -- default block quote color
 		vim.api.nvim_set_hl(0, 'MarkviewPalette1Sign', {link = 'markdownH1'})
+		vim.api.nvim_set_hl(0, '@markup.heading.1.markdown'     , {link = 'markdownH1'}) -- heading highlights in insert mode
 
 		return{
 			preview = {
