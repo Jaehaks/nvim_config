d6a1244439727403fdbb5c927b8e4edefe8b9f24
Jaehaks <wogkr3654@naver.com>
2025-07-03 17:47:43 +0900

    fix(init) : reorder calling modules in init
    At first opening, `autocmds` modules need to be loaded latter then
    keymaps and options. because these are basic operation, it has priority

--- init.lua
+++ init.lua
@@ -3,9 +3,9 @@ if vim.g.vscode then		-- vscode config is separated
 	require('jaehak.lazy_manager')
 else
 	-- require("jaehak.core")
-	require("jaehak.core.autocmds")
 	require("jaehak.core.keymaps")
 	require("jaehak.core.options")
+	require("jaehak.core.autocmds")
 	require("jaehak.core.utils")
 	require("jaehak.core.usercmds")
 	require("jaehak.lazy_manager")
