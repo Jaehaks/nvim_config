06b42109b0b57db1b4c021c70b1ff020b0e79182
Jaehaks <wogkr3654@naver.com>
2025-08-05 10:41:57 +0900

    feat(ime) : change keymap layout to english automatically in normal mode
    1. Problem
    
    When I return to normal mode after I edit some files with korean,
    I have to change ime mode to english for proper normal operation.
    
    2. Solution
    
    Make autocmd to change ime automatically when insertleave event is
    invoked.
    
    3. Limitation
    
    I want to change to normal mode using only characters like j,k instead
    of <Esc>. but it seems to be impossible. because I use three-row
    typewriter and korean word which is in same location of j,k is needed to
    use ㄲ.
    
    I think I must be satisfied to this solution

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ else
 	-- require("jaehak.core")
 	require("jaehak.core.keymaps")
 	require("jaehak.core.options")
+	require("jaehak.core.ime")
 	require("jaehak.core.autocmds")
 	require("jaehak.core.utils")
 	require("jaehak.core.usercmds")
---
+++ lua/jaehak/core/ime.lua
@@ -0,0 +1,81 @@
+-- Thank to kls1991 from https://github.com/keaising/im-select.nvim/issues/20
+
+-- TODO:
+-- When I type some Korean word in insert mode and out to normal mode using ESC,
+-- The korean IME is remained so I cannot normal operation directly. It must need to change IME to English
+-- This workflow is too annoying. So autocmd to change IME automatically is needed
+-- im-select.exe can be solution but it is too old repository and doesn't work.
+
+local ffi = require('ffi') -- get inherit ffi lua module to integrate c lang and la
+
+-- declare c definition
+ffi.cdef [[
+    typedef unsigned int UINT, HWND, WPARAM;
+    typedef unsigned long LPARAM, LRESULT;
+    LRESULT SendMessageA(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+    HWND ImmGetDefaultIMEWnd(HWND unnamedParam1);
+    HWND GetForegroundWindow();
+]]
+
+local user32 = ffi.load "user32.dll"
+local imm32 = ffi.load "imm32.dll"
+local ime_hwnd
+
+local WM_IME_CONTROL = 0x283
+local IMC_GETCONVERSIONMODE = 0x001
+local IMC_SETCONVERSIONMODE = 0x002
+
+-- Korean/English IME mode values
+-- It is tested by get_ime_mode() for each IME mode
+local ime_mode_kr = 1
+local ime_mode_en = 0
+
+-- set IME mode
+local function set_ime_mode(mode)
+    return user32.SendMessageA(ime_hwnd, WM_IME_CONTROL, IMC_SETCONVERSIONMODE, mode)
+end
+
+-- get IME mode
+local function get_ime_mode()
+    return user32.SendMessageA(ime_hwnd, WM_IME_CONTROL, IMC_GETCONVERSIONMODE, 0)
+end
+
+-- #############################################
+-- ### Set autocmd for transition of IME #######
+-- #############################################
+local ime_group = vim.api.nvim_create_augroup("ime_toggle", { clear = true })
+
+-- Get the IME handler associated with the Neovim window
+-- Runs only once when an InsertEnter/CmdlineEnter event occurs.
+vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
+    group = ime_group,
+    once = true,
+    desc = "Get ime control hwnd attached to nvim window",
+    callback = function()
+        ime_hwnd = imm32.ImmGetDefaultIMEWnd(user32.GetForegroundWindow())
+		vim.g.ime_mode_in_insert = get_ime_mode()
+    end,
+})
+
+-- Automatically switches IME to English mode when returning to Normal mode.
+vim.api.nvim_create_autocmd({ "InsertLeave", "CmdlineLeave" }, {
+    group = ime_group,
+    desc = "Switch IME to English mode when entering Normal mode",
+    callback = function()
+		vim.g.ime_mode_in_insert = get_ime_mode() -- save current IME mode
+        if ime_mode_kr == vim.g.ime_mode_in_insert then
+            set_ime_mode(ime_mode_en)
+        end
+    end,
+})
+
+-- Automatically switches IME to Korean mode when entering Insert mode.
+vim.api.nvim_create_autocmd("InsertEnter", {
+    group = ime_group,
+    desc = "Switch IME to Korean mode when entering insert mode",
+    callback = function()
+		if vim.g.ime_mode_in_insert ~= get_ime_mode() then
+			set_ime_mode(vim.g.ime_mode_in_insert)
+		end
+    end,
+})
