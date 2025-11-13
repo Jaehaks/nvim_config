37730f1eaf5424b15b86a208efd2ee8ab761cf8e
Jaehaks <wogkr3654@naver.com>
2025-11-13 14:49:18 +0900

    fix(hlchunk) : remove animation delay of chunk

--- lua/jaehak/plugins/hlchunk.lua
+++ lua/jaehak/plugins/hlchunk.lua
@@ -14,11 +14,13 @@ return {{
 			text                = true,
 		}
 		return {
-			chunk = {
+			chunk = { -- current chunk block using treesitter
 				enable = true,
+				duration = 0, -- animation time
+				delay = 50, -- debounce filter
 				exclude_filetypes = default_exclude_filetype,
 			},
-			indent = {
+			indent = { -- show all indent
 				enable = false,
 				exclude_filetypes = default_exclude_filetype,
 			},
@@ -32,8 +34,10 @@ return {{
 			}
 		}
 	end
-}
+},
 }
 --	'lukas-reineke/indent-blankline.nvim' 		  -->>>> works well, not support current indentation level, it depends on treesitter
 --	'nvimdev/indentmini.nvim' : loading time is fast, but not work
 --  'echasnovski/mini.indentscope' : I think it is slower than hlchunk. and hlchunk's graphic is more good
+--  'blink.indent' : author said it is blazing fast, but it doesn't reflect treesitter result,
+--  				 There are overlapping functions with hlchunk
