c2fd4c3f5ecb35e15e51660c670d6a683705cda0
Jaehaks <wogkr3654@naver.com>
2026-03-28 10:03:39 +0900

    fix(neogit) : use gitui.nvim as main git integration
    It is more faster than neogit, I migrate this integration from neogit

--- lua/jaehak/plugins/neogit.lua
+++ lua/jaehak/plugins/neogit.lua
@@ -2,7 +2,7 @@ return {
 {
 	"NeogitOrg/neogit",
 	keys = { -- negit loading time is too long
-		{'<leader>go', '<Cmd>Neogit<CR>', desc = 'open neogit default', mode = 'n'}
+		{'<leader>gO', '<Cmd>Neogit<CR>', desc = 'open neogit default', mode = 'n'}
 	},
 	dependencies = {
 		"sindrets/diffview.nvim",        -- optional - Diff integration
@@ -40,44 +40,9 @@ return {
 	}
 },
 {
-	-- install libgit2 : choco install libgit2
-	-- required : add 'rocks = {enabled = false}' to disable lazy.nvim's luarocks support
-	-- 			  to lazy.nvim's setting. because fugit2 can be builded by lua>5.1
-	'SuperBo/fugit2.nvim',
-	enabled = false,
-	build = false, -- It consider you have libgit2.dll already
-	cmd = {'Fugit2', 'Fugit2Diff', 'Fugit2Graph'},
-	keys = {
-		{ '<leader>gO', '<cmd>Fugit2<CR>' },
-	},
-	dependencies = {
-		'MunifTanjim/nui.nvim',
-		'nvim-tree/nvim-web-devicons',
-		'nvim-lua/plenary.nvim',
-		-- {
-		-- 	'chrisgrieser/nvim-tinygit', -- optional: for Github PR view
-		-- 	dependencies = { 'stevearc/dressing.nvim' }
-		-- },
-	},
-	opts = {
-		libgit2_path = require('jaehak.core.paths').nvim.fugit2 .. '\\libgit2.dll',
-		width = 100,
-		min_width = 50,
-		content_width = 60,
-		max_width = "80%",
-		height = "60%",
-		external_diffview = true, -- use diffview.nvim
-		blame_priority = 1,
-		blame_info_height = 10,
-		blame_info_width = 60,
-		show_patch = false,
-		command_timeout = 15000,
-	},
-},
-{
 	'Jaehaks/gitui.nvim',
 	keys = {
-		{'<leader>gO', function () require('gitui').open() end, desc = 'Open gitui'}
+		{'<leader>go', function () require('gitui').open() end, desc = 'Open gitui'}
 	},
 	opts = {
 
