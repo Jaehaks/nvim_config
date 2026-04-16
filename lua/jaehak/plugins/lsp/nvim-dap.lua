232a0dcad900a662950a2501c2e37a4808cdd548
Jaehaks <wogkr3654@naver.com>
2026-04-16 23:43:48 +0900

    feat(dap) : attach lsp to repl when debugging automatically
    attach lsp and set syntax to repl

--- lua/jaehak/plugins/lsp/nvim-dap.lua
+++ lua/jaehak/plugins/lsp/nvim-dap.lua
@@ -1,8 +1,94 @@
+
+-- ================================
+-- common functions
+-- ================================
+
+---@class dap_state
+---@field root string? project root absolute path
+---@field type string? dap type which is running
+local state = {
+	root = nil,
+	type = nil,
+}
+
+--- clear state
+local function clear_state()
+	state.root = nil
+	state.type = nil
+end
+
+---@type table<string, string> (dap type name, lsp name to attach to repl)
+local lsp_list = {
+	python = 'pyrefly',
+}
+
+--- find lsp id which is connected to buffer used in debugging session
+---@param root string
+---@return vim.lsp.Client?
+local function get_lsp_client(lsp_name, root)
+	local clients = vim.lsp.get_clients({ name = lsp_name})
+	for _, client in ipairs(clients) do
+		if client.root_dir and (vim.fs.normalize(client.root_dir) == vim.fs.normalize(root)) then
+			return client
+		end
+	end
+end
+
+--- get current root project with caching
+---@return string absolute path of dap root
+local function get_dap_root()
+	local utils = require('jaehak.core.utils')
+	if not state.root then state.root = utils.GetRoot(0) end
+	return state.root
+end
+
+--- configure common dap setting
+local function configure_dap_common(dap)
+
+	-- set global state
+	dap.listeners.before.launch['common'] = function(session)
+		state.root = get_dap_root()
+		state.type = session.config.type
+	end
+
+	-- clear global state
+	dap.listeners.after.event_terminated['common'] = clear_state
+	dap.listeners.after.disconnect['common']       = clear_state
+
+	-- attach lsp to repl and set keymap
+	vim.api.nvim_create_autocmd('FileType', {
+		group   = vim.api.nvim_create_augroup('nvim-dap-common', { clear = true }),
+		pattern = { 'dap-repl', 'dap-view' },
+		callback = function(args)
+			local session = dap.session()
+			if not session or session.config.type == 'matlab' then return end
+
+			local bufnr = args.buf
+			local lsp_name = lsp_list[state.type]
+			if lsp_name and state.root then
+				local client = get_lsp_client(lsp_name, state.root)
+				if client then
+					vim.lsp.buf_attach_client(bufnr, client.id)
+					vim.bo[bufnr].syntax = state.type
+					vim.diagnostic.enable(false, { bufnr = bufnr })
+				else
+					vim.notify('[dap] no LSP client: ' .. lsp_name, vim.log.levels.WARN)
+				end
+			end
+
+			vim.keymap.set('i', '<C-p>', '<Up>', { buffer = bufnr, remap = true, desc = '[dap] prev command history' })
+			vim.keymap.set('i', '<C-n>', '<Down>', { buffer = bufnr, remap = true, desc = '[dap] next command history' })
+		end,
+	})
+end
+
 -- ================================
 -- dap setting for python
 -- ================================
 
 local function configure_dap_python(dap)
+	local utils = require('jaehak.core.utils')
+
 	-- configure debug adapter
 	-- use debugpy which mason installed
 	dap.adapters.python = {
@@ -14,9 +100,10 @@ local function configure_dap_python(dap)
 		}
 	}
 
-	local utils = require('jaehak.core.utils')
 	-- /////////// configure launch //////////////
 	--- check the project uses uv
+	---@param root string
+	---@return boolean true if root is uv project
 	local is_uv = function (root)
 		return vim.fn.filereadable(utils.sep_unify(root .. '/uv.lock')) == 1 or
 				(vim.fn.filereadable(utils.sep_unify(root .. '/pyproject.toml')) == 1 and
@@ -24,47 +111,48 @@ local function configure_dap_python(dap)
 	end
 
 	--- set python path uv or general python
-	---@return string
+	---@return string python execution path
 	local python_path = function ()
-		local root = utils.GetRoot(0)
+		local root = get_dap_root()
 
-		-- if root doesn't have uv system, use general python
-		if not is_uv(root) then
+		-- if root doesn't be uv project, use general python
+		if not root or not is_uv(root) then
 			return 'python'
 		end
 
-		-- if root has uv, executes 'uv sync' to synchronize with pyproject.toml synchronously
+		-- xecutes 'uv sync' to synchronize with pyproject.toml synchronously
+		-- it failed, use general python
 		local obj = vim.system({'uv', 'sync'}, {text = true, cwd = root}):wait()
-		if obj.code == 0 then
-			vim.notify("uv sync successed", vim.log.levels.INFO)
-			local py = vim.g.has_win32 and "/.venv/Scripts/python" or "/.venv/bin/python"
-			local path = utils.sep_unify(root .. py)
-			return path
-		else
+		if obj.code ~= 0 then
 			vim.notify('uv sync failed', vim.log.levels.ERROR)
 			return 'python'
 		end
+
+		-- use python in uv project venv
+		vim.notify("uv sync successed", vim.log.levels.INFO)
+		local py = vim.g.has_win32 and "/.venv/Scripts/python" or "/.venv/bin/python"
+		return utils.sep_unify(root .. py)
 	end
 
 	--- set environment variables for uv
+	---@return table environment variables
 	local python_env = function ()
-		local root = utils.GetRoot(0)
-		if is_uv(root) then
-			local venv_path = utils.sep_unify(root .. '/.venv')
-			local bin_path = utils.sep_unify(venv_path .. (vim.g.has_win32 and '/Scripts' or '/bin'))
-			return {
-				VIRTUAL_ENV = venv_path,
-				PATH = bin_path .. ';' .. vim.env.PATH,
-			}
-		end
-		return {}
+		local root = get_dap_root()
+		if not root or not is_uv(root) then return {} end
+
+		local venv_path = utils.sep_unify(root .. '/.venv')
+		local bin_path = utils.sep_unify(venv_path .. (vim.g.has_win32 and '/Scripts' or '/bin'))
+		return {
+			VIRTUAL_ENV = venv_path,
+			PATH = bin_path .. ';' .. vim.env.PATH,
+		}
 	end
 
 	-- [config] launch without args
 	local launch_without_args = {
 		type = 'python',
 		request = 'launch',
-		name = "Launch file",
+		name = "Launch current file",
 		program = "${file}",
 		pythonPath = python_path,
 		env = python_env,
@@ -75,8 +163,32 @@ local function configure_dap_python(dap)
 		-- REPL / internalConsole is output only. it cannot be used for input.
 	}
 
+	local launch_with_args = {
+		type = 'python',
+		request = 'launch',
+		name = "Launch current file:args",
+		program = "${file}",
+		args = function ()
+			local args_string = vim.fn.input({
+				prompt = 'Arguments: ',
+				completion = 'file',
+				cancelreturn = '', -- emtpy value if user cancel
+			})
+			-- if argument is empty, run without args
+			if not args_string or args_string == '' then
+				return {}
+			end
+			vim.fn.histadd('input', args_string) -- save to 'input'(@) register.
+			return require("dap.utils").splitstr(args_string)
+		end,
+		pythonPath = python_path,
+		env = python_env,
+		console = "integratedTerminal",
+	}
+
 	dap.configurations.python = {
 		launch_without_args,
+		launch_with_args,
 	}
 end
 
@@ -91,17 +203,22 @@ return {
 	config = function ()
 		local dap = require('dap')
 		-- dap.set_log_level('TRACE')
-		-- ================================
-		-- set dap for python
-		-- ================================
-		configure_dap_python(dap)
 
+		-- ================================
+		-- dap configuration
+		-- ================================
+		configure_dap_common(dap) -- for all languages excepts of matlab
+		configure_dap_python(dap) -- for python
+
+		-- ================================
+		-- override dap notify
+		-- ================================
 		local dap_utils = require('dap.utils')
 		local origin_notify = dap_utils.notify
 		---@diagnostic disable-next-line: duplicate-set-field
 		dap_utils.notify = function (msg, log_level)
 			if log_level == vim.log.levels.WARN then
-				-- disable warning message when python debugger is terminated forcibly
+				-- 1) python : disable warning message when python debugger is terminated forcibly
 				if msg:match('exited with 1') and msg:match('debugpy') then
 					return
 				end
