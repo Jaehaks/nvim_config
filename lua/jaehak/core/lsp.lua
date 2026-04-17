b394d7c0ab0a649743f9dadef7bd4bd23111a0b6
Jaehaks <wogkr3654@naver.com>
2026-04-17 23:16:39 +0900

    fix(lsp) : More exclude set to efficient python lsp
    sometimes cursor move is not smooth in python file,
    I anticipate that it is too many unnecessary feature loading due to
    multiple lsp.
    
    So it needs to separate their features over than before.
    
    If the feature is related with textchange or cursormove like
    completionProvider / documentHighlightProvider / semantiTokenProvker /
    documentSymbolProvider.

--- lua/jaehak/core/lsp.lua
+++ lua/jaehak/core/lsp.lua
@@ -329,11 +329,20 @@ vim.lsp.config('basedpyright', {
 	filetypes = {'python'},
 	root_dir = root_dir_python,
 	on_attach = function (client, _)
+		-- these will be executed whenever cursor moving / typing.
 		client.server_capabilities.completionProvider        = false -- use pyrefly for fast response
-		client.server_capabilities.definitionProvider        = false -- use pyrefly for fast response
 		client.server_capabilities.documentHighlightProvider = false -- use pyrefly for fast response
-		client.server_capabilities.renameProvider            = false -- use pyrefly as I think it is stable
+		client.server_capabilities.documentSymbolProvider    = false -- use pyrefly for fast response
 		client.server_capabilities.semanticTokensProvider    = false -- use pyrefly it is more rich
+
+		-- formatting to ruff
+		client.server_capabilities.documentFormattingProvider = false -- use ruff
+		client.server_capabilities.documentRangeFormattingProvider = false -- use ruff
+
+		client.server_capabilities.definitionProvider        = false -- use pyrefly for fast response
+		client.server_capabilities.declarationProvider        = false -- use pyrefly for fast response
+		client.server_capabilities.typeDefinitionProvider    = false -- use pyrefly
+		client.server_capabilities.renameProvider            = false -- use pyrefly as I think it is stable
 	end,
 	settings = { -- see https://docs.basedpyright.com/latest/configuration/language-server-settings/
 		basedpyright = {
@@ -369,12 +378,14 @@ vim.lsp.config('pyrefly', {
 	filetypes = {'python'},
 	root_dir = root_dir_python,
 	on_attach = function (client, _)
-		client.server_capabilities.codeActionProvider     = false -- basedpyright has more kinds
-		client.server_capabilities.documentSymbolProvider = false -- basedpyright has more kinds
-		client.server_capabilities.hoverProvider          = false -- basedpyright has more kinds
-		client.server_capabilities.inlayHintProvider      = false -- basedpyright has more kinds
-		client.server_capabilities.referenceProvider      = false -- basedpyright has more kinds
-		client.server_capabilities.signatureHelpProvider  = false -- basedpyright has more kinds
+		client.server_capabilities.codeActionProvider      = false -- basedpyright has more kinds
+		client.server_capabilities.hoverProvider           = false -- basedpyright has more kinds
+		client.server_capabilities.inlayHintProvider       = false -- basedpyright has more kinds
+		client.server_capabilities.referencesProvider      = false -- basedpyright has more kinds
+		client.server_capabilities.signatureHelpProvider   = false -- basedpyright has more kinds
+		client.server_capabilities.workspaceSymbolProvider = false -- basedpyright has more kinds
+		client.server_capabilities.implementationProvider = false -- basedpyright has more kinds
+		client.server_capabilities.callHierarchyProvider  = false -- basedpyright has more kinds
 
 		if vim.g.has_win32 then
 			table.insert(pid.pyrefly, require('jaehak.core.utils').GetProcessId('pyrefly.exe'))
