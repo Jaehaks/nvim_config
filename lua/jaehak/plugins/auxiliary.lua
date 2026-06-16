f6737d7b4f4880b923bdf556e4f745378e5828bb
Jaehaks <wogkr3654@naver.com>
2026-06-16 23:25:08 +0900

    fix(aux) : add matlab custom icon for nvim-web-devicons

--- lua/jaehak/plugins/auxiliary.lua
+++ lua/jaehak/plugins/auxiliary.lua
@@ -6,5 +6,12 @@ return {
 {
 	"nvim-tree/nvim-web-devicons", -- If you want devicons
 	lazy = true,
+	opts = {
+		override_by_extension = {
+			['m'] = { icon = '', color = '#FF853B', name = 'matlab' },
+			['mat'] = { icon = '', color = '#FF853B', name = 'matlab' },
+			['dat'] = { icon = '', color = '#721080', name = 'data' },
+		}
+	}
 },
 }
